b53a3a78055855d96e41531dea265be5f126061b
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-01-30 12:21:54 -0500

    added basic struct layout

---
+++ BackendPkg/Ingredient.go
@@ -0,0 +1,11 @@
+package Backend
+
+type Ingredient struct{
+	item FoodItem
+	amount float64
+	unitOfMeasurement string
+}
+
+func CalculatePrice(food Ingredient) float64{
+	// Take in food item and return its price
+}
