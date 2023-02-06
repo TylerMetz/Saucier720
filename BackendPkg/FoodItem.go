97e4a882195abec334761ff1de6b050515e05144
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-02-06 14:37:39 -0500

    more changes

--- BackendPkg/FoodItem.go
+++ BackendPkg/FoodItem.go
@@ -1,21 +1,19 @@
 package Backend
 
-import "fmt"
-
-type FoodItem struct{
-	name string
+type FoodItem struct {
+	name      string
 	storeCost float64
-	onSale bool
+	onSale    bool
 	salePrice float64
 }
 
 // generates new FoodItem with defaults set to 0 values, used for pantry items since only name matters
-func NewPantryItem(name string) *FoodItem{
-	
+func NewPantryItem(name string) *FoodItem {
+
 	item := FoodItem{name: name}
 	item.storeCost = 0
 	item.onSale = false
 	item.salePrice = 0
 
-	return &FoodItem
+	return &item
 }
