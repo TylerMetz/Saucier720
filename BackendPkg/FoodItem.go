3cc4210e6fba6f09754e0a9560a82be55b952912
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-02-06 15:20:43 -0500

    changed all struct variables to capital letters

--- BackendPkg/FoodItem.go
+++ BackendPkg/FoodItem.go
@@ -1,19 +1,19 @@
 package BackendPkg
 
 type FoodItem struct {
-	name      string
-	storeCost float64
-	onSale    bool
-	salePrice float64
+	Name      string
+	StoreCost float64
+	OnSale    bool
+	SalePrice float64
 }
 
 // generates new FoodItem with defaults set to 0 values, used for pantry items since only name matters
 func NewPantryItem(name string) *FoodItem {
 
-	item := FoodItem{name: name}
-	item.storeCost = 0
-	item.onSale = false
-	item.salePrice = 0
+	item := FoodItem{Name: name}
+	item.StoreCost = 0
+	item.OnSale = false
+	item.SalePrice = 0
 
 	return &item
 }
