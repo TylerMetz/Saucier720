aef324c3365cf606a8545e5a77a66cb3c11dd67b
Tyler Metz <tmbs9963@gmail.com>
2023-02-06 16:54:48 -0500

    has title with Pantry

--- Saucier720-app/src/app/app.module.ts
+++ Saucier720-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { PantryComponent } from './pantry/pantry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantryComponent
   ],
   imports: [
     BrowserModule
