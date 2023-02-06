1b806da6ff335cbae2009aa10675e789628732b8
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-02-06 14:37:39 -0500

    more changes

--- BackendPkg/Recipe.go
+++ BackendPkg/Recipe.go
@@ -1,18 +1,20 @@
 package Backend
 
-type Recipe struct{
+type Recipe struct {
 	// List of steps
 	// List of inegredients
-	name string 
-	servings int
-	time int
-	cost float64
+	name        string
+	servings    int
+	time        int
+	cost        float64
 	servingSize int
 }
 
-func CalcTotalPrice(/*Need to decide data structure*/) float64{
+func CalcTotalPrice( /*Need to decide data structure*/ ) float64 {
 	// Calculates total price of a recipe
+	panic("placeholder")
 }
-func CalcServing(/*Need to decide data structure*/) float64{
-	// Calculates total price of a recipe given accounting for size 
+func CalcServing( /*Need to decide data structure*/ ) float64 {
+	// Calculates total price of a recipe given accounting for size
+	panic("placeholder")
 }
