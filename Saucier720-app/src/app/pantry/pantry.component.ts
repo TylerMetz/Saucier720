508de8d0a0ecf7d5acfbdcbb2b2f362900a930fc
Tyler Metz <tmbs9963@gmail.com>
2023-02-06 16:54:48 -0500

    has title with Pantry

---
+++ Saucier720-app/src/app/pantry/pantry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantry',
+  templateUrl: './pantry.component.html',
+  styleUrls: ['./pantry.component.scss']
+})
+export class PantryComponent {
+
+}
