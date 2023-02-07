ba5a73ee756d5528ff440a612a1662c31fc48ca4
Edward Menello <edmenello@gmail.com>
2023-02-07 12:51:30 -0500

    Added recipe and deals component

--- Saucier720-app/src/app/list/list.component.ts
+++ Saucier720-app/src/app/list/list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../core/interfaces/ingredient';
+import { IngredientService } from '../core/services/ingredient.service';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  ingredients: Array<Ingredient> = [];
 
+  constructor(private ingredientService: IngredientService) { }
+
+  ngOnInit(){
+    this.ingredients = this.ingredientService.getPantry();
+  }
 }
