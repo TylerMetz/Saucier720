4c19b7a3066ac79aa430b989481f2ec238613a92
Edward Menello <edmenello@gmail.com>
2023-02-07 12:32:17 -0500

    Started top nav bar

--- Saucier720-app/src/app/app.module.ts
+++ Saucier720-app/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PantryComponent } from './pantry/pantry.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PantryComponent
+    PantryComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
