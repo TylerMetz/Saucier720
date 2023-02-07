17b6518a26317c00cadab7204839762e73952b93
Edward Menello <edmenello@gmail.com>
2023-02-07 13:02:05 -0500

    Copied the pantry page into the other 4 pages

--- Saucier720-app/src/app/recipes/recipes.component.ts
+++ Saucier720-app/src/app/recipes/recipes.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../core/interfaces/ingredient';
+import { IngredientService } from '../core/services/ingredient.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss']
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit{
+  ingredients: Array<Ingredient> = [];
 
+  constructor(private ingredientService: IngredientService) { }
+
+  ngOnInit(){
+    this.ingredients = this.ingredientService.getPantry();
+  }
 }
