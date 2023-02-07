5982d87c09d71caa019b5ce1332b4ea3d82fd2d4
Edward Menello <edmenello@gmail.com>
2023-02-07 16:26:06 -0500

    App routing

--- Saucier720-app/src/app/app.module.ts
+++ Saucier720-app/src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PantryComponent } from './pantry/pantry.component';
-import { ListComponent } from './list/list.component';
-import { DealsComponent } from './deals/deals.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    PantryComponent,
-    ListComponent,
-    DealsComponent,
-    RecipesComponent,
+    routingComponents,
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
