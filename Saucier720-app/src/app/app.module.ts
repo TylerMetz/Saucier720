3e889eb601357e3a3665e82b821164ef96f23833
Edward Menello <edmenello@gmail.com>
2023-02-07 12:51:30 -0500

    Added recipe and deals component

--- Saucier720-app/src/app/app.module.ts
+++ Saucier720-app/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PantryComponent } from './pantry/pantry.component';
 import { ListComponent } from './list/list.component';
+import { DealsComponent } from './deals/deals.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PantryComponent,
-    ListComponent
+    ListComponent,
+    DealsComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
