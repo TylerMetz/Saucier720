1ba319e0361c036fadb39c77a34fbeef6f573068
Tyler Metz <tmbs9963@gmail.com>
2023-02-15 13:31:32 -0500

    basic testing setup

--- Saucier720-app/src/app/testing/app.component.spec.ts
+++ Saucier720-app/src/app/testing/app.component.spec.ts
@@ -1,31 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from '../app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+      declarations: [ AppComponent ]
+    })
+    .compileComponents();
+
+  fixture = TestBed.createComponent(AppComponent);
+  component = fixture.componentInstance;
+  fixture.detectChanges
+});
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'Saucier720-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Saucier720-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Saucier720-app app is running!');
+    expect(component).toBeTruthy();
   });
 });
