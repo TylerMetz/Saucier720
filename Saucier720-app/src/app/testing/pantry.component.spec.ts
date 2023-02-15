d11cb0321eea0edeb0a44086ae7d4ac6dd21a812
Tyler Metz <tmbs9963@gmail.com>
2023-02-15 12:41:22 -0500

    setting up testing

--- Saucier720-app/src/app/testing/pantry.component.spec.ts
+++ Saucier720-app/src/app/testing/pantry.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PantryComponent } from './pantry.component';
+import { PantryComponent } from '../pantry/pantry.component';
 
 describe('PantryComponent', () => {
   let component: PantryComponent;
