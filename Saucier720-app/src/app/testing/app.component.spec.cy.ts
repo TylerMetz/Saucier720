bf82477b5727db607e1f1f64a290a8b2e65d303e
Tyler Metz <tmbs9963@gmail.com>
2023-02-22 12:19:35 -0500

    script for cypress and docs

--- Saucier720-app/src/app/testing/app.component.spec.cy.ts
+++ Saucier720-app/src/app/testing/app.component.spec.cy.ts
@@ -2,21 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from '../app.component';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
-    })
-    .compileComponents();
-
-  fixture = TestBed.createComponent(AppComponent);
-  component = fixture.componentInstance;
-  fixture.detectChanges
-});
-
-  it('should create the app', () => {
-    expect(component).toBeTruthy();
-  });
+  it('mounts', () => {
+    cy.mount(AppComponent)
+  })
 });
--- Saucier720-app/src/app/testing/list.component.spec.cy.ts
+++ Saucier720-app/src/app/testing/list.component.spec.cy.ts
@@ -1,23 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListComponent } from '../list/list.component';
 
 describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('mounts', () => {
+    cy.mount(ListComponent)
+  })
 });
