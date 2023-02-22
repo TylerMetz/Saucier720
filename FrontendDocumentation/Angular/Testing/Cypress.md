353cf1762f03c94dbdb7c8d6760e0994581e13c9
Tyler Metz <tmbs9963@gmail.com>
2023-02-22 12:19:35 -0500

    script for cypress and docs

---
+++ FrontendDocumentation/Angular/Testing/Cypress.md
@@ -0,0 +1,4 @@
+# Cypress
+- the testing frameowrk we are using is Cypress
+- it is run in the browser 
+- run `./cypress.sh` to open up the browser and start running tests
--- Saucier720-app/src/app/testing/pantry.component.spec.cy.ts
+++ Saucier720-app/src/app/testing/pantry.component.spec.cy.ts
@@ -1,23 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PantryComponent } from '../pantry/pantry.component';
 
 describe('PantryComponent', () => {
-  let component: PantryComponent;
-  let fixture: ComponentFixture<PantryComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PantryComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(PantryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('mounts', () => {
+    cy.mount(PantryComponent)
+  })
 });
