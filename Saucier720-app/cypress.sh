a4f46fe6040d1715e21e6609951eb909f4e95efa
Tyler Metz <tmbs9963@gmail.com>
2023-02-23 14:16:46 -0500

    format

--- Saucier720-app/cypress.sh
+++ Saucier720-app/cypress.sh
@@ -1,4 +1,1 @@
-# cypress is configured in Saucier720-app there are two versions of 
-#this script so you can execute from both directories
-cd Saucier720-app
 npx cypress open
--- cypress.sh
+++ cypress.sh
@@ -1,1 +1,4 @@
+# cypress is configured in Saucier720-app there are two versions of 
+#this script so you can execute from both directories
+cd Saucier720-app
 npx cypress open
