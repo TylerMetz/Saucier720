3e48fd22e2ac5c552f5c455fabff95d81216be23
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-02-23 04:49:18 -0500

    It works with selenium, need to add the Load More button inputs

--- main.go
+++ main.go
@@ -2,101 +2,106 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
 )
 
 func main() {
-	// Start a Selenium WebDriver server instance (if one is not already
-	// running).
-	const (
-		seleniumPath    = "src/SeleniumDrivers/selenium-server-4.8.1.jar"
-		geckoDriverPath = "src/SeleniumDrivers/chromedriver_mac64/chromedriver"
-		port            = 8080
-	)
-	opts := []selenium.ServiceOption{
-		selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
-		selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox.
-		selenium.Output(os.Stderr),            // Output debug information to STDERR.
-	}
-	selenium.SetDebug(true)
-	service, err := selenium.NewSeleniumService(seleniumPath, port, opts...)
+	opts := []selenium.ServiceOption{}
+	service, err := selenium.NewChromeDriverService("src/SeleniumDrivers/chromedriver_mac64/chromedriver", 9515, opts...)
 	if err != nil {
-		panic(err) // panic is used only as an example and is not otherwise recommended.
+		panic(err)
 	}
 	defer service.Stop()
 
-	// Connect to the WebDriver instance running locally.
-	caps := selenium.Capabilities{"browserName": "chrome"}
-	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+	caps := selenium.Capabilities{
+		"browserName": "chrome",
+	}
+	chromeCaps := chrome.Capabilities{
+		Args: []string{
+			"--headless",
+			"--disable-gpu",
+			"--no-sandbox",
+		},
+	}
+	caps.AddChrome(chromeCaps)
+
+	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", 9515))
 	if err != nil {
 		panic(err)
 	}
 	defer wd.Quit()
 
-	// Go to the Publix website
-	if err := wd.Get("https://www.publix.com/savings/weekly-ad/view-all"); err != nil {
-		panic(err)
-	}
-
-	// Click the "Choose a store" button.
-	btn, err := wd.FindElement(selenium.ByCSSSelector, ".p-button button--default button--lg button--primary")
-	if err != nil {
-		panic(err)
-	}
-	if err := btn.Click(); err != nil {
-		panic(err)
-	}
-
-    // KEY ENTRY
-
-    // tab down to search bar
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.TabKey)
-
-    // enter zip code, gnv by default
-    selenium.KeyDownAction("3")
-    selenium.KeyDownAction("2")
-    selenium.KeyDownAction("6")
-    selenium.KeyDownAction("0")
-    selenium.KeyDownAction("1")
-
-    // go to search button and select
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.EnterKey)
-
-    // tab down to "Choose Store" button and hit enter
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.TabKey)
-    selenium.KeyDownAction(selenium.EnterKey)
-
-	// Wait for the program to finish running and get the output.
-	outputDiv, err := wd.FindElement(selenium.ByCSSSelector, "p-grid-item")
+	err = wd.Get("https://www.publix.com/savings/weekly-ad/view-all")
 	if err != nil {
 		panic(err)
 	}
 
-	var output string
-	for {
-		output, err = outputDiv.Text()
-		if err != nil {
-			panic(err)
-		}
-		if output != "Waiting for remote server..." {
-			break
-		}
-		time.Sleep(time.Millisecond * 100)
+	chooseStoreButton, err := wd.FindElement(selenium.ByCSSSelector, "#main > div.savings-content-wrapper.skeleton-spacer > div > div.savings-container.full-bleed > div > div > button > span")
+	if err != nil {
+		panic(err)
+	}
+	err = chooseStoreButton.Click()
+	if err != nil {
+		panic(err)
+	} else{
+		fmt.Println("landing page button selected")
 	}
 
-	fmt.Printf("%s", strings.Replace(output, "\n\n", "\n", -1))
+	inputBox, err := wd.FindElement(selenium.ByCSSSelector, "#main > div:nth-child(5) > div > div > div.content.no-padding > div.p-store-locator > div > div > div > form > input[type=search]")
+	if err != nil {
+		panic(err)
+	}
+	err = inputBox.SendKeys("32601")
+	if err != nil {
+		panic(err)
+	} else{
+		fmt.Println("zip inputed")
+	}
 
+	searchStoreButton, err := wd.FindElement(selenium.ByCSSSelector, "#main > div:nth-child(5) > div > div > div.content.no-padding > div.p-store-locator > div > div > div.search-container > form > button")
+	if err != nil {
+		panic(err)
+	}
+	err = searchStoreButton.Click()
+	if err != nil {
+		panic(err)
+	} else{
+		fmt.Println("search button pressed")
+	}
+	time.Sleep(20 * time.Second) // wait for page to load
 
+	chooseStoreResult, err := wd.FindElement(selenium.ByCSSSelector, "#\\31 560 > div > div > div.buttons-links > div.p-button-group__wrapper.buttons-wrapper > div > button")
+	if err != nil {
+		panic(err)
+	}
+	err = chooseStoreResult.Click()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("store selected")
+	}
+	time.Sleep(5 * time.Second) // wait for page to load
+
+	listViewButton, err := wd.FindElement(selenium.ByCSSSelector, "#listing")
+	if err != nil {
+		panic(err)
+	}
+	err = listViewButton.Click()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("list view enabled")
+	}
+
+	time.Sleep(20 * time.Second) // wait for page to load
+
+	html, err := wd.PageSource()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(html)
 }
