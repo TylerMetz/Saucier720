72edf092f3598e3fe4f9c5dfd6e8c38363e36ad9
Tyler Metz <tmbs9963@gmail.com>
2023-02-26 14:18:55 -0500

    working towards http requests

--- Saucier720-app/src/app/pantry/pantry.component.ts
+++ Saucier720-app/src/app/pantry/pantry.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../core/interfaces/ingredient';
-import { IngredientService } from '../core/services/ingredient.service';
+import { Pantry } from '../core/interfaces/pantry';
+import { PantryService } from '../core/services/pantry/pantry.service';
 
 @Component({
   selector: 'app-pantry',
   templateUrl: './pantry.component.html',
+  providers: [PantryService],
   styleUrls: ['./pantry.component.scss']
 })
 
 export class PantryComponent implements OnInit {
-  ingredients: Array<Ingredient> = [];
+  pantry: Pantry | undefined;
 
-  constructor(private ingredientService: IngredientService) { }
+  constructor(private pantryService: PantryService) { }
 
   ngOnInit(){
-    this.ingredients = this.ingredientService.getPantry();
+    //this.pantry = this.pantryService.getPantry();
   }
 
+  showPantry() {
+    this.pantryService.getPantry()
+      .subscribe((data: Pantry) => this.pantry = { ...data });
+  }
 }
