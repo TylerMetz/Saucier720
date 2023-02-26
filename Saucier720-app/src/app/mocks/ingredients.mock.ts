95344641696425bc8a0180d3dddacbb715681651
Tyler Metz <tmbs9963@gmail.com>
2023-02-26 15:39:13 -0500

    updated html and pantry interface

--- Saucier720-app/src/app/mocks/ingredients.mock.ts
+++ Saucier720-app/src/app/mocks/ingredients.mock.ts
@@ -1,14 +1,5 @@
 import { Ingredient } from "../core/interfaces/ingredient";
 
 export const PANTRY: Array<Ingredient> = [
-    {name: 'peanut butter',
-    quantity: 1,
-    price: 1.99},
-    {name: 'jelly',
-    quantity: 1,
-    price: 1.99},
-    {name: 'bread',
-    quantity: 1,
-    price: 1.99},
-    
-];
+    {name: 'bread', price: 1.99, onSale: true, quantity: 1, saleDetails: 'bah'}
+]
