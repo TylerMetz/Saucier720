9ed3d565a9771fb95951c105db504a887c0f6f94
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-02-28 14:53:38 -0500

    finished unit testing

--- src/BackendPkg/Router.go
+++ src/BackendPkg/Router.go
@@ -1,48 +1,48 @@
 package BackendPkg
 
 import (
-    "fmt"
-    "log"
-    "net/http"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 	"github.com/rs/cors"
-    "github.com/gorilla/mux"
 )
 
-type Router struct{
-	Name string
+type Router struct {
+	Name             string
 	ItemsToBeEncoded []interface{}
 }
 
-func (t *Router) Rout() { 
-    
+func (t *Router) Rout() {
+
 	// creates new router
 	route := mux.NewRouter()
 	route.HandleFunc("/api/Pantry", t.sendResponse)
 
 	// enables alternate hosts for CORS
 	c := cors.New(cors.Options{
-        AllowedOrigins: []string{"http://localhost:4200"},
-        AllowCredentials: true,
-    })
+		AllowedOrigins:   []string{"http://localhost:4200"},
+		AllowCredentials: true,
+	})
 
 	log.Println("Listening...")
 	handler := c.Handler(route)
-	http.ListenAndServe(":8080", handler) 
+	http.ListenAndServe(":8080", handler)
 }
 
 func (t *Router) sendResponse(response http.ResponseWriter, request *http.Request) {
-	
+
 	jsonResponse, jsonError := json.Marshal(t.ItemsToBeEncoded)
-	
+
 	if jsonError != nil {
-	fmt.Println("Unable to encode JSON")
+		fmt.Println("Unable to encode JSON")
 	}
-	
-	fmt.Println(string(jsonResponse))
-	
+
+	// fmt.Println(string(jsonResponse)) // used to test
+
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusOK)
 	response.Write(jsonResponse)
 }
-
