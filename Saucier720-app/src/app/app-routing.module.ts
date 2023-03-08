8d138aeb6ec2bf0cf2c8029529c113a011e85522
Edward Menello <edmenello@gmail.com>
2023-03-08 13:49:57 -0500

    Created login and signup components and added bootstrap button

--- Saucier720-app/src/app/app-routing.module.ts
+++ Saucier720-app/src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { ListComponent } from './list/list.component';
 import { PantryComponent } from './pantry/pantry.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: 'Pantry', component: PantryComponent},
   { path: 'List', component: ListComponent},
   { path: 'Deals', component: DealsComponent},
-  { path: 'Recipes', component: RecipesComponent}
+  { path: 'Recipes', component: RecipesComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
