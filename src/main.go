cd9fd6263ffe46368ff8a1dcc41830a3cf280158
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-03-08 14:15:09 -0500

    New branch for organizing scraped deals

--- src/main.go
+++ src/main.go
@@ -6,38 +6,40 @@ import (
 )
 
 func main() {
-		testFoodItem := BackendPkg.FoodItem{
-			Name:        "peanut butter",
-			StoreCost:   369.99,
-			OnSale:      true,
-			SaleDetails: "BOGO",
-			Quantity:    10,
-		}
-		testFoodItem2 := BackendPkg.FoodItem{
-			Name:        "jelly",
-			StoreCost:   1.0,
-			OnSale:      false,
-			SaleDetails: "N/A",
-			Quantity:    30,
-		}
-		testFoodItem3 := BackendPkg.FoodItem{
-			Name:        "bread",
-			StoreCost:   10.69,
-			OnSale:      true,
-			SaleDetails: "$2 for 2",
-			Quantity:    2,
-		}
+		// Organize string obtained from scraping data 
 
-		testFoodSlice := []interface{}{testFoodItem, testFoodItem2, testFoodItem3}
+		// testFoodItem := BackendPkg.FoodItem{
+		// 	Name:        "peanut butter",
+		// 	StoreCost:   369.99,
+		// 	OnSale:      true,
+		// 	SaleDetails: "BOGO",
+		// 	Quantity:    10,
+		// }
+		// testFoodItem2 := BackendPkg.FoodItem{
+		// 	Name:        "jelly",
+		// 	StoreCost:   1.0,
+		// 	OnSale:      false,
+		// 	SaleDetails: "N/A",
+		// 	Quantity:    30,
+		// }
+		// testFoodItem3 := BackendPkg.FoodItem{
+		// 	Name:        "bread",
+		// 	StoreCost:   10.69,
+		// 	OnSale:      true,
+		// 	SaleDetails: "$2 for 2",
+		// 	Quantity:    2,
+		// }
 
-		programRouter := BackendPkg.Router{
-			Name:             "testRouter",
-			ItemsToBeEncoded: testFoodSlice,
-		}
-		programRouter.Rout()
+		// testFoodSlice := []interface{}{testFoodItem, testFoodItem2, testFoodItem3}
+
+		// programRouter := BackendPkg.Router{
+		// 	Name:             "testRouter",
+		// 	ItemsToBeEncoded: testFoodSlice,
+		// }
+		// programRouter.Rout()
 
 	// runs scraper
-	runScraper := false
+	runScraper := true
 	if runScraper {
 		// create new groccery store
 		userPublix := BackendPkg.GroceryStore{
