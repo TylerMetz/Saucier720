c18d28a9d463a905d121bf784a3d467d38d7ac9c
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-03-10 13:40:03 -0500

    f

--- src/BackendPkg/Database.go
+++ src/BackendPkg/Database.go
@@ -1,17 +1,6 @@
-import (
-    "database/sql"
-    "github.com/mattn/go-sqlite3"
-)
+package BackendPkg
 
-func main(){
-	database, _ := sql.Opeen("sqllite3", "./Publix.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS FoodItems (
-		Name TEXT,
-		StoreCost REAL,
-		OnSale INTEGER,
-		SalePrice REAL,
-		SaleDetails TEXT,
-		Quantity INTEGER
-	)")
-	statement.Exec();
-}
+import (
+    //"database/sql"
+    //"github.com/mattn/go-sqlite3"
+)
