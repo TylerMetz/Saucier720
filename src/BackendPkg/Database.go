46f1afb217f37fee8e4ce5982bde3b128318f4ce
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-03-10 16:05:06 -0500

    added user sql

--- src/BackendPkg/Database.go
+++ src/BackendPkg/Database.go
@@ -7,18 +7,46 @@ import (
 
 type Database struct{
 	Name string
-	Store GroceryStore
 }
 
-func (d *Database) FoodItemSliceTest (f []FoodItem){
-	database, _ := sql.Open("sqlite3", "./Publix.db")
+// initializes application database file
+func (d *Database) OpenDatabase() *sql.DB{
+	database, _ := sql.Open("sqlite3", "./MealDealz.db")
+	return database
+}
+
+// need to pass in the inventory slice from the grocery store item
+func (d *Database) StorePublixDatabase (f []FoodItem){
+
+	// calls function to open the database
+	database := d.OpenDatabase()
+
 	// make table for food item data
 	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS FoodItems (Name TEXT, StoreCost REAL, OnSale INTEGER, SalePrice REAL, SaleDetails TEXT, Quantity INTEGER)")
 	statement.Exec();
+
 	//insert into food item table
 	statementTwo, _ := database.Prepare("INSERT INTO FoodItems (Name, StoreCost, OnSale, SalePrice, SaleDetails, Quantity) VALUES (?, ?, ?, ?, ?, ?)")
 
 	for _, item := range f {
 		statementTwo.Exec(item.Name, item.StoreCost, item.OnSale, item.SalePrice, item.SaleDetails, item.Quantity)
 	}
+}
+
+func (d *Database) StoreUserDatabase (u User){
+	
+	// calls function to open the database
+	database := d.OpenDatabase()
+
+	// make table for user data
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS UserData (FirstName TEXT, LastName TEXT, Email TEXT, UserName TEXT, Password TEXT)")
+	statement.Exec();
+
+	// insert into UserData table
+	statementTwo, _ := database.Prepare("INSERT INTO UserData (FirstName, LastName, Email, UserName, Password) VALUES (?, ?, ?, ?, ?)")
+
+	u.PrintUserInfo()
+	// store data from this user into table
+	statementTwo.Exec(u.FirstName, u.LastName, u.Email, u.UserName, u.Password)
+	
 }
