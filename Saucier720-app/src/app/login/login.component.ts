07c4ad12b430081c347699a75a76fd46fd67faec
Edward Menello <edmenello@gmail.com>
2023-03-13 15:05:41 -0400

    More changes to login page

--- Saucier720-app/src/app/login/login.component.ts
+++ Saucier720-app/src/app/login/login.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit{
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  ngOnInit(): void {
+    
+  }
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
 }
