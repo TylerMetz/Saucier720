e201b4802fe5cf0c2e40e939dad02cb2b99aa3fa
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-03-13 18:42:29 -0400

    push to db and user pantry test

--- src/main.go
+++ src/main.go
@@ -8,36 +8,46 @@ import (
 
 func main(){
 	
-	testFoodItem := BackendPkg.FoodItem{
-		Name:        "peanut butter",
-		StoreCost:   369.99,
-		OnSale:      true,
-		SaleDetails: "BOGO",
-		Quantity:    10,
-	}
-	testFoodItem2 := BackendPkg.FoodItem{
-		Name:        "jelly",
-		StoreCost:   1.0,
-		OnSale:      false,
-		SaleDetails: "N/A",
-		Quantity:    30,
-	}
-	testFoodItem3 := BackendPkg.FoodItem{
-		Name:        "bread",
-		StoreCost:   10.69,
-		OnSale:      true,
-		SaleDetails: "$2 for 2",
-		Quantity:    2,
-	}
+	// testFoodItem := BackendPkg.FoodItem{
+	// 	Name:        "peanut butter",
+	// 	StoreCost:   369.99,
+	// 	OnSale:      true,
+	// 	SaleDetails: "BOGO",
+	// 	Quantity:    10,
+	// }
+	// testFoodItem2 := BackendPkg.FoodItem{
+	// 	Name:        "jelly",
+	// 	StoreCost:   1.0,
+	// 	OnSale:      false,
+	// 	SaleDetails: "N/A",
+	// 	Quantity:    30,
+	// }
+	// testFoodItem3 := BackendPkg.FoodItem{
+	// 	Name:        "bread",
+	// 	StoreCost:   10.69,
+	// 	OnSale:      true,
+	// 	SaleDetails: "$2 for 2",
+	// 	Quantity:    2,
+	// }
 
-	testFoodSlice := []BackendPkg.FoodItem{testFoodItem, testFoodItem2, testFoodItem3}
+	//testFoodSlice := []BackendPkg.FoodItem{testFoodItem, testFoodItem2, testFoodItem3}
 	
 
 	// test scraper
-	runScraper := false
+	//Putting variables outsde for the sake of testing db
+	userPublix := BackendPkg.GroceryStore{
+		Name:    "Publix",
+		ZipCode: "32601",
+	}
+
+	// setup user groccery store
+	programScraper := BackendPkg.Scraper{
+		Store: userPublix,
+	}
+	runScraper := true
 	if runScraper {
 		// create new groccery store
-		userPublix := BackendPkg.GroceryStore{
+		/*userPublix := BackendPkg.GroceryStore{
 			Name:    "Publix",
 			ZipCode: "32601",
 		}
@@ -45,7 +55,7 @@ func main(){
 		// setup user groccery store
 		programScraper := BackendPkg.Scraper{
 			Store: userPublix,
-		}
+		}*/
 
 		// scrape all data
 		programScraper.Scrape()
@@ -55,14 +65,14 @@ func main(){
 
 		// Takes 48634 'Words' to get to the first items name...
 		// Testing to see if we can grab the name and deal from the function 
-		fmt.Println("Start")
-		programScraper.Store.OrganizeDeals(programScraper.DealsHTML, 48634, 48635)
-		//fmt.Println(deal)
-		//fmt.Println(name)
+		//testFoodSlice = programScraper.Store.OrganizeDeals(programScraper.DealsHTML, 48634)
 		fmt.Println("Finished")
 
+		//Print the scraper data
 		//fmt.Println(programScraper.DealsHTML)
 	}
+
+	testFoodSlice := programScraper.Store.OrganizeDeals(programScraper.DealsHTML, 48645)
 
 	// test database
 	testDatabase := BackendPkg.Database{
