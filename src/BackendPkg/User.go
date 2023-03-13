f479752f50a67f8ebc30d0dd2ac999809c832055
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-03-13 16:29:09 -0400

    updated from develop

---
+++ src/BackendPkg/User.go
@@ -0,0 +1,25 @@
+package BackendPkg
+
+import(
+	"fmt"
+)
+
+
+type User struct{
+	FirstName string
+	LastName string
+	Email string
+	UserName string
+	Password string
+	UserPantry Pantry
+}
+
+func (u* User) PrintUserInfo(){
+	fmt.Println("First Name: " + u.FirstName)
+	fmt.Println("Last Name: " + u.LastName)
+	fmt.Println("Email: " + u.Email)
+	fmt.Println("UserName: " + u.UserName)
+	fmt.Println("Password: " + u.Password)
+	u.UserPantry.DisplayPantry()
+}
+
--- src/main.go
+++ src/main.go
@@ -3,43 +3,38 @@ package main
 import (
 	"BackendPkg"
 	"fmt"
+	"time"
 )
 
-func main() {
-	// Organize string obtained from scraping data
+func main(){
+	
+	testFoodItem := BackendPkg.FoodItem{
+		Name:        "peanut butter",
+		StoreCost:   369.99,
+		OnSale:      true,
+		SaleDetails: "BOGO",
+		Quantity:    10,
+	}
+	testFoodItem2 := BackendPkg.FoodItem{
+		Name:        "jelly",
+		StoreCost:   1.0,
+		OnSale:      false,
+		SaleDetails: "N/A",
+		Quantity:    30,
+	}
+	testFoodItem3 := BackendPkg.FoodItem{
+		Name:        "bread",
+		StoreCost:   10.69,
+		OnSale:      true,
+		SaleDetails: "$2 for 2",
+		Quantity:    2,
+	}
 
-	// testFoodItem := BackendPkg.FoodItem{
-	// 	Name:        "peanut butter",
-	// 	StoreCost:   369.99,
-	// 	OnSale:      true,
-	// 	SaleDetails: "BOGO",
-	// 	Quantity:    10,
-	// }
-	// testFoodItem2 := BackendPkg.FoodItem{
-	// 	Name:        "jelly",
-	// 	StoreCost:   1.0,
-	// 	OnSale:      false,
-	// 	SaleDetails: "N/A",
-	// 	Quantity:    30,
-	// }
-	// testFoodItem3 := BackendPkg.FoodItem{
-	// 	Name:        "bread",
-	// 	StoreCost:   10.69,
-	// 	OnSale:      true,
-	// 	SaleDetails: "$2 for 2",
-	// 	Quantity:    2,
-	// }
+	testFoodSlice := []BackendPkg.FoodItem{testFoodItem, testFoodItem2, testFoodItem3}
+	
 
-	// testFoodSlice := []interface{}{testFoodItem, testFoodItem2, testFoodItem3}
-
-	// programRouter := BackendPkg.Router{
-	// 	Name:             "testRouter",
-	// 	ItemsToBeEncoded: testFoodSlice,
-	// }
-	// programRouter.Rout()
-
-	// runs scraper
-	runScraper := true
+	// test scraper
+	runScraper := false
 	if runScraper {
 		// create new groccery store
 		userPublix := BackendPkg.GroceryStore{
@@ -68,5 +63,40 @@ func main() {
 
 		//fmt.Println(programScraper.DealsHTML)
 	}
+
+	// test database
+	testDatabase := BackendPkg.Database{
+		Name: "MealDealz Database",
+	}
+	// store publix data to .db file
+	testDatabase.StorePublixDatabase(testFoodSlice)
+
+	// create a test user and store their pantry
+	testUser := BackendPkg.User{
+		FirstName: "Eddie",
+		LastName: "Menello",
+		Email: "Edward@gmail.com",
+		UserName: "Eddiefye69",
+		Password: "ILoveGraham420",
+		UserPantry: BackendPkg.Pantry{
+			FoodInPantry: testFoodSlice,
+			TimeLastUpdated: time.Now(),
+		},
+	}
+	testDatabase.StoreUserDatabase(testUser)
+	testDatabase.StoreUserPantry(testUser)
+
+	// read from .db file and output test user's pantry to frontend
+	var testFoodInterface []interface{}
+	for i := 0; i < len(testDatabase.GetUserPantry(testUser.UserName).FoodInPantry); i++{
+		testFoodInterface = append(testFoodInterface, testDatabase.GetUserPantry(testUser.UserName).FoodInPantry[i])
+	}
+
+	// test router
+	programRouter := BackendPkg.Router{
+		Name:             "testRouter",
+		ItemsToBeEncoded: testFoodInterface,
+	}
+	programRouter.Rout()
 
 }
