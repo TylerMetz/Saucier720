c55d9bcdc040408852e9c1a4721370a599ef1c06
Tyler Metz <tmbs9963@gmail.com>
2023-03-24 21:52:24 -0400

    updated Pantry to use bootstrap and template table

--- Saucier720-app/src/app/pantry/pantry.component.ts
+++ Saucier720-app/src/app/pantry/pantry.component.ts
@@ -1,41 +1,11 @@
-import { HttpEvent, HttpEventType } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { PantryService } from '../core/services/pantry/pantry.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pantry',
   templateUrl: './pantry.component.html',
-  providers: [PantryService],
   styleUrls: ['./pantry.component.scss']
 })
 
-export class PantryComponent implements OnInit {
-  pantry: any;
+export class PantryComponent {
 
-  constructor(private pantryService: PantryService) { }
-
-  ngOnInit(){
-    this.populatePantry();
-  }
-
-  populatePantry(): void {
-    this.pantryService.getPantry()
-      .subscribe((event: HttpEvent<any>) => {
-        switch(event.type) {
-          case HttpEventType.Sent:
-            console.log('Request sent!');
-            break;
-          case HttpEventType.ResponseHeader:
-            console.log('Response header received!');
-            break;
-            case HttpEventType.DownloadProgress:
-              const kbLoaded = Math.round(event.loaded / 1024);
-              console.log(`Download in progress! ${kbLoaded}Kb loaded`);
-              break;
-            case HttpEventType.Response:
-              console.log('Done!', event.body);
-              this.pantry = event.body;
-        }
-      }); //doesnt call until subscribed
-  }
 }
