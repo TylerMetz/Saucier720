a23cc5c5d73a0dd7648bded456ea370d9db22ec7
Tyler Metz <tmbs9963@gmail.com>
2023-03-24 23:38:08 -0400

    Updated button to post example data

--- Saucier720-app/src/app/pantry/FEC/new-pantry-item-button/new-pantry-item-button.component.ts
+++ Saucier720-app/src/app/pantry/FEC/new-pantry-item-button/new-pantry-item-button.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-pantry-item-button',
-  templateUrl: './new-pantry-item-button.component.html',
-  styleUrls: ['./new-pantry-item-button.component.scss']
+  template: '<button (click)="postPantryItem()">Submit</button>',
 })
 export class NewPantryItemButtonComponent {
+  pantryPostUrl = 'http://localhost:8082/api/NewPantryItem'
 
+  constructor(private http: HttpClient) { }
+
+  postPantryItem() {
+    const req = new HttpRequest('POST', this.pantryPostUrl, { 
+      reportProgress: true
+    });
+
+  }
 }
