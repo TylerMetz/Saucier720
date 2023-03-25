d5fae274cfad491d6f0043e9f1c9e03877d259f8
Tyler Metz <tmbs9963@gmail.com>
2023-03-24 23:30:25 -0400

    generated new component

---
+++ Saucier720-app/src/app/pantry/FEC/new-pantry-item-button/new-pantry-item-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-pantry-item-button',
+  templateUrl: './new-pantry-item-button.component.html',
+  styleUrls: ['./new-pantry-item-button.component.scss']
+})
+export class NewPantryItemButtonComponent {
+
+}
