4250947ddd3453bafec9c38a77f6e7f10c95b5ee
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-03-28 12:28:06 -0400

    backend testing

---
+++ Saucier720-app/src/app/testing/list.component.spec.cy.ts
@@ -0,0 +1,8 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ListComponent } from '../list/list.component';
+
+describe('ListComponent', () => {
+  it('mounts', () => {
+    cy.mount(ListComponent)
+  })
+});
