b5bd9870e63b5a284d6a57b035ee2764f2b49c53
Tyler Metz <tmbs9963@gmail.com>
2023-03-29 18:21:21 -0400

    PostVideo

--- goTest.sh
+++ goTest.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 # Everytime a new go test file is created/modified just change underneath
 cd src/TestingFiles
 go test Host_test.go
 go test Scrape_test.go
+go test Db_test.go
+go test DealsDb_test.go
