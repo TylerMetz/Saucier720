2ab1d3f156b2894014c80a487c0017f501e0af11
Tyler Metz <tmbs9963@gmail.com>
2023-03-30 18:16:19 -0400

    updated pantry mock and created deals mock

--- Saucier720-app/src/app/mocks/pantry.mock.ts
+++ Saucier720-app/src/app/mocks/pantry.mock.ts
@@ -1,8 +1,44 @@
 import { Ingredient } from "../core/interfaces/ingredient";
 
+    var mockIngredient1: Ingredient = {
+        "Name":"riley butter",
+        "StoreCost":369.99,
+        "OnSale":true,
+        "SalePrice":0,
+        "SaleDetails":"BOGO",
+        "Quantity":10
+    };
+
+    var mockIngredient2: Ingredient = {
+        "Name":"peanut butter",
+        "StoreCost":369.99,
+        "OnSale":true,
+        "SalePrice":0,
+        "SaleDetails":"BOGO",
+        "Quantity":10
+    };
+
+    var mockIngredient3: Ingredient = {
+        "Name":"jelly",
+        "StoreCost":1,
+        "OnSale":false,
+        "SalePrice":0,
+        "SaleDetails":"N/A",
+        "Quantity":30
+    };
+    
+    var mockIngredient4: Ingredient = {
+        "Name":"bread",
+        "StoreCost":10.69,
+        "OnSale":true,
+        "SalePrice":0,
+        "SaleDetails":"$2 for 2",
+        "Quantity":2
+    };
+
 export const PANTRY: Ingredient[] = [
-    {"Name":"riley butter","StoreCost":369.99,"OnSale":true,"SalePrice":0,"SaleDetails":"BOGO","Quantity":10},
-    {"Name":"peanut butter","StoreCost":369.99,"OnSale":true,"SalePrice":0,"SaleDetails":"BOGO","Quantity":10},
-    {"Name":"jelly","StoreCost":1,"OnSale":false,"SalePrice":0,"SaleDetails":"N/A","Quantity":30},
-    {"Name":"bread","StoreCost":10.69,"OnSale":true,"SalePrice":0,"SaleDetails":"$2 for 2","Quantity":2}
+    mockIngredient1,
+    mockIngredient2,
+    mockIngredient3,
+    mockIngredient4,
 ];
