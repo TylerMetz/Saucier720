1936aba7beafcd3a64b7ce94efda61bc0f1ff4ec
Tyler Metz <tmbs9963@gmail.com>
2023-03-30 12:43:45 -0400

    Flushes the deals with the mock pantry right now

--- Saucier720-app/src/app/testing/deals/deals.component.spec.cy.ts
+++ Saucier720-app/src/app/testing/deals/deals.component.spec.cy.ts
@@ -3,12 +3,42 @@ import { TestBed, inject } from '@angular/core/testing';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { DealsComponent } from '../../deals/deals.component';
+import { PANTRY } from 'src/app/mocks/pantry.mock';
 
 describe('DealsComponent', () => {
+
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ DealsService ]
+    });
     cy.mount(DealsComponent);
   })
   it('displays table', () =>{
     cy.get('app-deals-table');
   });
+
+  it('should get pantry',
+  inject(
+    [HttpTestingController, DealsService],
+    (httpMock: HttpTestingController, dealsService: DealsService) => {
+      dealsService.getDeals().subscribe((event: HttpEvent<any>) => {
+        switch (event.type) {
+          case HttpEventType.Response:
+            expect(event.body).equal(PANTRY);
+        }
+      });
+
+      const mockReq = httpMock.expectOne(dealsService.dealsUrl);
+
+      expect(mockReq.cancelled).to.equal(false);
+      expect(mockReq.request.responseType).to.equal('json');
+      mockReq.flush(PANTRY);
+
+      httpMock.verify();
+
+    }
+  ));
+
+
 });
