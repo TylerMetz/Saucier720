72e947c5617f39b3d2063d8e52af614bd8f6f385
Tyler Metz <tmbs9963@gmail.com>
2023-03-30 18:18:18 -0400

    updated deals mock

--- Saucier720-app/src/app/mocks/deals.mock.ts
+++ Saucier720-app/src/app/mocks/deals.mock.ts
@@ -1,5 +1,44 @@
-import { Ingredient } from "../core/interfaces/ingredient"
+import { Ingredient } from "../core/interfaces/ingredient";
+
+    var mockDeals1: Ingredient = {
+        "Name":"riley butter",
+        "StoreCost":369.99,
+        "OnSale":true,
+        "SalePrice":0,
+        "SaleDetails":"BOGO",
+        "Quantity":10
+    };
+
+    var mockDeals2: Ingredient = {
+        "Name":"peanut butter",
+        "StoreCost":369.99,
+        "OnSale":true,
+        "SalePrice":0,
+        "SaleDetails":"BOGO",
+        "Quantity":10
+    };
+
+    var mockDeals3: Ingredient = {
+        "Name":"jelly",
+        "StoreCost":1,
+        "OnSale":false,
+        "SalePrice":0,
+        "SaleDetails":"N/A",
+        "Quantity":30
+    };
+    
+    var mockDeals4: Ingredient = {
+        "Name":"bread",
+        "StoreCost":10.69,
+        "OnSale":true,
+        "SalePrice":0,
+        "SaleDetails":"$2 for 2",
+        "Quantity":2
+    };
 
 export const DEALS: Ingredient[] = [
-    
-]
+    mockDeals1,
+    mockDeals2,
+    mockDeals3,
+    mockDeals4,
+];
