21c01f6abe6685696b9deafd32f892e382a57b04
Tyler Metz <tmbs9963@gmail.com>
2023-04-02 13:36:11 -0400

    pantry table async

--- Saucier720-app/src/app/pantry/FEC/pantry-table/pantry-table.component.ts
+++ Saucier720-app/src/app/pantry/FEC/pantry-table/pantry-table.component.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpEventType } from "@angular/common/http"
 import { Component, OnInit } from '@angular/core';
 import { PantryService } from 'src/app/core/services/pantry/pantry.service';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-pantry-table',
@@ -13,28 +14,31 @@ export class PantryTableComponent implements OnInit {
 
   constructor(private pantryService: PantryService) { }
 
-  ngOnInit(){
-    this.populatePantry();
-  }
-  populatePantry(): void {
-    this.pantryService.getPantry()
-      .subscribe((event: HttpEvent<any>) => {
-        switch(event.type) {
-          case HttpEventType.Sent:
-            console.log('Request sent!');
-            break;
-          case HttpEventType.ResponseHeader:
-            console.log('Response header received!');
-            break;
-            case HttpEventType.DownloadProgress:
-              const kbLoaded = Math.round(event.loaded / 1024);
-              console.log(`Download in progress! ${kbLoaded}Kb loaded`);
-              break;
-            case HttpEventType.Response:
-              console.log('Done!', event.body);
-              this.pantry = event.body;
-        }
-      }); //doesnt call until subscribed
+  async ngOnInit(){
+    await this.populatePantry();
   }
 
+  async populatePantry(): Promise<void> {
+    try {
+      const event: HttpEvent<any> = await lastValueFrom(this.pantryService.getPantry());
+      switch(event.type) {
+        case HttpEventType.Sent:
+          console.log('Request sent!');
+          break;
+        case HttpEventType.ResponseHeader:
+          console.log('Response header received!');
+          break;
+        case HttpEventType.DownloadProgress:
+          const kbLoaded = Math.round(event.loaded / 1024);
+          console.log(`Download in progress! ${kbLoaded}Kb loaded`);
+          break;
+        case HttpEventType.Response:
+          console.log('Done!', event.body);
+          this.pantry = event.body;
+          break;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
