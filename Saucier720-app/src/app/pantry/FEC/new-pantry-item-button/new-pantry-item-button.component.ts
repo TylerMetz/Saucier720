0fa5b669445c9cf728c60813911ae42ebdd6a85e
Tyler Metz <tmbs9963@gmail.com>
2023-04-02 13:24:05 -0400

    updated new item button to not use subscribe

--- Saucier720-app/src/app/pantry/FEC/new-pantry-item-button/new-pantry-item-button.component.ts
+++ Saucier720-app/src/app/pantry/FEC/new-pantry-item-button/new-pantry-item-button.component.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpEvent } from '@angular/common/http';
 import { PantryService } from 'src/app/core/services/pantry/pantry.service';
 import { PANTRY } from 'src/app/mocks/pantry.mock';
 
-
 @Component({
   selector: 'app-new-pantry-item-button',
   templateUrl: './new-pantry-item-button.component.html',
@@ -15,16 +14,12 @@ export class NewPantryItemButtonComponent {
 
   constructor(private pantryService: PantryService) { }
 
-postPantryItem() {
-  this.pantryService.postPantryItem(PANTRY[0])
-    .subscribe(
-      response => {
-        console.log(response);
-      },
-      error => {
-        console.error(error);
-      }
-    );
-}
-
+  async postPantryItem() {
+    try {
+      const response = await this.pantryService.postPantryItem(PANTRY[0]).toPromise();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
