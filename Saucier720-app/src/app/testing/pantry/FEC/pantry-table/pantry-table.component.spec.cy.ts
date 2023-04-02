09edc050195d1420f0dceef7905357c9f8516ca2
Tyler Metz <tmbs9963@gmail.com>
2023-04-02 14:48:01 -0400

    no http testing in components

--- Saucier720-app/src/app/testing/pantry/FEC/pantry-table/pantry-table.component.spec.cy.ts
+++ Saucier720-app/src/app/testing/pantry/FEC/pantry-table/pantry-table.component.spec.cy.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { PANTRY } from 'src/app/mocks/pantry.mock';
 import { PantryTableComponent } from '../../../../pantry/FEC/pantry-table/pantry-table.component';
@@ -9,12 +8,11 @@ describe('PantryTableComponent', () => {
   let component: PantryTableComponent;
   let fixture: ComponentFixture<PantryTableComponent>;
   let pantryService: PantryService;
-  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PantryTableComponent],
-      imports: [HttpClientTestingModule],
+      imports: [],
       providers: [PantryService]
     }).compileComponents();
   });
