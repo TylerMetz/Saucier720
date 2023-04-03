9d749fe6d9a023f73fcd2eb32ee17e0dbede25dd
Tyler Metz <tmbs9963@gmail.com>
2023-04-02 20:40:49 -0400

    Authorization service

---
+++ Saucier720-app/src/app/core/services/Auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthService {
+
+  private loggedIn = false;
+
+  private loginUrl = 'api/login';
+  private logoutUrl = 'api/logout';
+
+  constructor(private http: HttpClient) { }
+
+  public login(username: string, password: string): Observable<any> {
+    return this.http.post(this.loginUrl, { username, password });
+  }
+
+  public logout(): Observable<any> {
+    return this.http.post(this.logoutUrl, {});
+  }
+
+  public isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
+
+}
