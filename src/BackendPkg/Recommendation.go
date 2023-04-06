db9dd03908d631a6f01f62d89e8bb518750a1877
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-04-06 00:13:46 -0400

    further organizing for reciper suggestions

--- src/BackendPkg/Recommendation.go
+++ src/BackendPkg/Recommendation.go
@@ -16,6 +16,7 @@ func BestRecipes(userPantry Pantry, allRecipes []Recipe, deals []FoodItem) []Rec
 	
 	var scoreList []int
 	
+	// ranks recipes based off of what is in pantry 
 	for i := 0; i < len(userPantry.FoodInPantry); i++{
 		for j:= 0; j < len(allRecipes); j++{
 			tempScore := 0
@@ -45,12 +46,22 @@ func BestRecipes(userPantry Pantry, allRecipes []Recipe, deals []FoodItem) []Rec
     // copy the first 30 indices (or all of the indices, if there are fewer than 30) into the highestScoreIndices slice
     copy(highestScoreIndices, indices[:min(30, len(indices))])
 
+	// finding corresponding recipes at highest scoring indices 
 	for m := 0; m < len(allRecipes); m++{
-		var pantryItemsInRecipe []FoodItem
-		var dealsItemsInRecipe []FoodItem
 		for n := 0; n < len(highestScoreIndices); n++{
 			if(m == highestScoreIndices[n]){
 				// get items in pantry
+				var pantryItemsInRecipe []FoodItem
+				var dealsItemsInRecipe []FoodItem
+
+				// check which food items are actually contained in recipe
+				for i := 0; i < len(userPantry.FoodInPantry); i++{
+					for j:=0; j < len(allRecipes[m].Ingredients); j++{
+						if(strings.Contains(allRecipes[m].Ingredients[j],userPantry.FoodInPantry[i].Name)){
+							pantryItemsInRecipe = append(pantryItemsInRecipe, userPantry.FoodInPantry[i])
+						}
+					}
+				}
 				newRecc := Reccomendation{
 					r: allRecipes[m],
 					itemsInPantry: pantryItemsInRecipe,
