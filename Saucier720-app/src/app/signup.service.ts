a52ba302b500a4709874a677802ebee378a5806c
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-04-06 13:58:00 -0400

    mock user included

--- Saucier720-app/src/app/signup.service.ts
+++ Saucier720-app/src/app/signup.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+let mockUser: any = {
+  "FirstName":"Eddie",
+  "LastName":"Menello",
+  "Email":"Edward@gmail.com",
+  "UserName":"Eddiefye69",
+  "Password":"ILoveGraham420",
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor() { }
+  private signupUrl: string = "api/Signup"
+
+  constructor(private http: HttpClient) {}
+
+  public signup(): Observable<any>{
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { mockUser };
+    return this.http.post<any>(this.signupUrl, body, { headers, withCredentials: true });
+  }
 }
