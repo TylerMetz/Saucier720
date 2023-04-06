43d188de88561eee4f2c9c15804682ee3b428186
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-04-06 13:55:36 -0400

    combined functions in main

--- src/main.go
+++ src/main.go
@@ -82,7 +82,7 @@ func main(){
 	testUserFoodSlice := []BackendPkg.FoodItem{testFoodItem, testFoodItem2, testFoodItem3, testFoodItem4, testFoodItem5, testFoodItem6, testFoodItem7, testFoodItem8, testFoodItem9, testFoodItem10}
 
 	// test database
-	testDatabase := BackendPkg.Database{
+	programDatabase := BackendPkg.Database{
 		Name: "MealDealz Database",
 	}
 
@@ -112,33 +112,28 @@ func main(){
 	}
 
 	// store Eddie
-	testDatabase.StoreUserDatabase(testUser)
-	testDatabase.StoreUserPantry(testUser)
+	programDatabase.StoreUserDatabase(testUser)
+	programDatabase.StoreUserPantry(testUser)
 
 	// store Eddie version of Sam
-	testDatabase.StoreUserDatabase(testUserTwo)
-	testDatabase.StoreUserPantry(testUserTwo)
+	programDatabase.StoreUserDatabase(testUserTwo)
+	programDatabase.StoreUserPantry(testUserTwo)
 
-	// Reads recipes dataset
-	testDatabase.WriteRecipes()
+	// Reads recipes dataset in not read in yet and stores in DB
+	programDatabase.WriteRecipes()
 
 	// runs scraper if new deals at publix
-	CheckIfScrapeNewDeals(testDatabase)
+	CheckIfScrapeNewDeals(programDatabase)
 
-	// routs deals to deals page
-	go RoutWeeklyDeals(testDatabase)
+	RoutAllData(programDatabase, testUser)
 
-	// routs Eddie's pantry, lol
-	go RoutUserPantry(testDatabase, testUser)
-
-	testReccList := BackendPkg.BestRecipes(testDatabase.GetUserPantry("Eddiefye69"), testDatabase.ReadRecipes(), testDatabase.ReadPublixDatabase())
-
-	for i := 0; i < len(testReccList); i++{
+	//testReccList := BackendPkg.BestRecipes(programDatabase.GetUserPantry("Eddiefye69"), programDatabase.ReadRecipes(), programDatabase.ReadPublixDatabase())
+	/*for i := 0; i < len(testReccList); i++{
 		fmt.Println(testReccList[i].R.Title)
 		for j := 0; j < len(testReccList[i].ItemsInPantry); j++{
 			fmt.Println(testReccList[i].ItemsInPantry[j].Name)
 		}
-	}
+	}*/
 
 	BackendPkg.ListenForAllPosts();
 }
@@ -218,4 +213,31 @@ func RoutWeeklyDeals(d BackendPkg.Database){
 		ItemsToBeEncoded: testFoodInterface,
 	}
 	go programRouter.Rout("/api/Deals", ":8081")
+}
+
+func RoutReccommendedRecipes(d BackendPkg.Database, currUser BackendPkg.User){
+
+	userReccList := BackendPkg.BestRecipes(d.GetUserPantry(currUser.UserName), d.ReadRecipes(), d.ReadPublixDatabase())
+	var testFoodInterface []interface{}
+	for i := 0; i < len(userReccList); i++{
+		testFoodInterface = append(testFoodInterface, userReccList[i])
+	}
+
+	programRouter := BackendPkg.Router{
+		Name:             "testRouter",
+		ItemsToBeEncoded: testFoodInterface,
+	}
+	go programRouter.Rout("/api/Recipes", ":8082")
+}
+
+func RoutAllData(d BackendPkg.Database, currUser BackendPkg.User){
+
+	// routs Eddie's pantry, lol
+	go RoutUserPantry(d, currUser)
+
+	// routs deals to deals page
+	go RoutWeeklyDeals(d)
+
+	// routs reccommended recipes to recipes page
+	go RoutReccommendedRecipes(d, currUser)
 }
