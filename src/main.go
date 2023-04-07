66184a33efddc24f62514b2dc9e7c9feba4fc6a0
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-04-07 17:06:13 -0400

    organized interface

--- src/main.go
+++ src/main.go
@@ -219,7 +219,15 @@ func RoutRecommendedRecipes(d BackendPkg.Database, currUser BackendPkg.User){
 	userRecList := BackendPkg.BestRecipes(d.GetUserPantry(currUser.UserName), d.ReadRecipes(), d.ReadPublixDatabase())
 	var testFoodInterface []interface{}
 	for i := 0; i < len(userRecList); i++{
-		testFoodInterface = append(testFoodInterface, userRecList[i])
+		testFoodInterface = append(testFoodInterface, userRecList[i].R)
+		testFoodInterface = append(testFoodInterface, "Pantry Data:")
+		for j := 0; j < len(userRecList[i].ItemsInPantry); j++{
+			testFoodInterface = append(testFoodInterface, userRecList[i].ItemsInPantry[j].Name)
+		}
+		testFoodInterface = append(testFoodInterface, "Publix Data:")
+		for k := 0; k < len(userRecList[i].ItemsOnSale); k++{
+			testFoodInterface = append(testFoodInterface, userRecList[i].ItemsOnSale[k].Name)
+		}
 	}
 
 	programRouter := BackendPkg.Router{
