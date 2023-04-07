bfd843e27ae364aeaa2d31ab50aef9d3b39ba28c
emenello <edmenello@gmail.com>
2023-04-07 19:08:50 -0400

    signup form

---
+++ Saucier720-app/src/app/signup/FEC/signup-form/signup-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.scss']
+})
+export class SignupFormComponent {
+
+}
