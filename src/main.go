9d375bed9f334bb62a02da2b053712cfc9063ab3
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-04-07 14:38:56 -0400

    new submod

--- src/main.go
+++ src/main.go
@@ -128,8 +128,9 @@ func main(){
 	// routs all data
 	go RoutAllData(programDatabase, testUser)
 
-	//testReccList := BackendPkg.BestRecipes(programDatabase.GetUserPantry("Eddiefye69"), programDatabase.ReadRecipes(), programDatabase.ReadPublixDatabase())
-	
+	testReccList := BackendPkg.BestRecipes(programDatabase.GetUserPantry("Eddiefye69"), programDatabase.ReadRecipes(), programDatabase.ReadPublixDatabase())
+	BackendPkg.OutputRecommendations(testReccList)
+
 	BackendPkg.ListenForAllPosts();
 	
 }
