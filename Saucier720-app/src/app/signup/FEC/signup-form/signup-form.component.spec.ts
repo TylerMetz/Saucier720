f52135ea422b25440e79940f7ae7a23769fc0743
emenello <edmenello@gmail.com>
2023-04-07 19:08:50 -0400

    signup form

---
+++ Saucier720-app/src/app/signup/FEC/signup-form/signup-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignupFormComponent } from './signup-form.component';
+
+describe('SignupFormComponent', () => {
+  let component: SignupFormComponent;
+  let fixture: ComponentFixture<SignupFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SignupFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SignupFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
