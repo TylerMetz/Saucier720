389d01cd3b8c3b7898297b9878d5a1ed764009ff
emenello <edmenello@gmail.com>
2023-04-07 23:09:19 -0400

    update form

--- Saucier720-app/src/app/pantry/FEC/new-pantry-item-button/new-pantry-item-button.component.ts
+++ Saucier720-app/src/app/pantry/FEC/new-pantry-item-button/new-pantry-item-button.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PantryService } from 'src/app/core/services/pantry/pantry.service';
 import { PANTRY } from 'src/app/mocks/pantry.mock';
 import { lastValueFrom } from 'rxjs';
+import { Ingredient } from 'src/app/core/interfaces/ingredient';
 
 @Component({
   selector: 'app-new-pantry-item-button',
@@ -10,10 +11,24 @@ import { lastValueFrom } from 'rxjs';
 })
 
 export class NewPantryItemButtonComponent {
+  name: string = '';
+  storeCost: number = 100;
+  onSale: boolean = true;
+  salePrice: number = 0;
+  saleDetails: string = '';
+  quantity: number = 1;
 
   constructor(private pantryService: PantryService) { }
 
   async postPantryItem() {
+    const user: Ingredient = {
+      Name: this.name,
+      StoreCost: this.storeCost,
+      OnSale: this.onSale,
+      SalePrice: this.salePrice,
+      SaleDetails: this.saleDetails,
+      Quantity: this.quantity,
+    };
     try {
       const response = await lastValueFrom(this.pantryService.postPantryItem(PANTRY[0]));
       console.log(response);
