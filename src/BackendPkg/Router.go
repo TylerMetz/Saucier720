1e30f949d406bd342f40e3733a6f0fd39fc659fe
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-04-11 18:19:04 -0400

    removed commented out code

--- src/BackendPkg/Router.go
+++ src/BackendPkg/Router.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
 	//"time"
 	"io/ioutil"
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 	"log"
 	"sync"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	//"strings"
 )
 
@@ -22,7 +24,7 @@ var dealsFoodInterface []interface{}
 var recipesFoodInterface []interface{}
 
 type Router struct {
-	Name             string
+	Name string
 }
 
 func (t *Router) RoutPantry(endLink string, port string) {
@@ -127,9 +129,8 @@ func ListenPantry(currUser User) {
 
 	route := mux.NewRouter()
 	route.HandleFunc("/api/NewPantryItem", func(w http.ResponseWriter, r *http.Request) {
-        PantryItemPostResponse(w, r, currUser)
-    })
-
+		PantryItemPostResponse(w, r, currUser)
+	})
 
 	// enables alternate hosts for CORS
 	c := cors.New(cors.Options{
@@ -138,16 +139,16 @@ func ListenPantry(currUser User) {
 	})
 
 	handler := c.Handler(route)
-    log.Fatal(http.ListenAndServe(":8083", handler))
+	log.Fatal(http.ListenAndServe(":8083", handler))
 
 }
 
 func PantryItemPostResponse(w http.ResponseWriter, r *http.Request, currUser User) {
 
 	if r.Method != "POST" {
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-        return
-    }
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
 	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
@@ -156,26 +157,26 @@ func PantryItemPostResponse(w http.ResponseWriter, r *http.Request, currUser Use
 		FoodItem FoodItem `json:"ingredient"`
 	}
 
-    var newItem Ingredient;
-	
-    err = json.Unmarshal(body, &newItem)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+	var newItem Ingredient
+
+	err = json.Unmarshal(body, &newItem)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	funcDatabase := Database{
 		Name: "func db",
 	}
-    funcDatabase.InsertPantryItemPost(currUser, newItem.FoodItem)
+	funcDatabase.InsertPantryItemPost(currUser, newItem.FoodItem)
 
 	w.WriteHeader(http.StatusOK)
 
-	if http.StatusOK == 200{
+	if http.StatusOK == 200 {
 		d := Database{
 			Name: "func db",
-		} 
-		
+		}
+
 		var testFoodInterfaceRefresh []interface{}
 		testFoodInterface = testFoodInterfaceRefresh
 		UpdateData(d, currUser)
@@ -187,7 +188,7 @@ func ListenNewUser() {
 
 	// Listens and Serves New User
 	route := mux.NewRouter()
-    route.HandleFunc("/api/Signup", NewUserResponse).Methods("POST")
+	route.HandleFunc("/api/Signup", NewUserResponse).Methods("POST")
 
 	// enables alternate hosts for CORS
 	c := cors.New(cors.Options{
@@ -196,47 +197,47 @@ func ListenNewUser() {
 	})
 
 	handler := c.Handler(route)
-    log.Fatal(http.ListenAndServe(":8085", handler))
+	log.Fatal(http.ListenAndServe(":8085", handler))
 
 }
 
 func NewUserResponse(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "POST" {
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-        return
-    }
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-    body, err := ioutil.ReadAll(r.Body)
+	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 
-    type UserResponse struct {
+	type UserResponse struct {
 		User User `json:"user"`
 	}
 
 	var newUser UserResponse
-	
-    err = json.Unmarshal(body, &newUser)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+
+	err = json.Unmarshal(body, &newUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	funcDatabase := Database{
 		Name: "func db",
 	}
 
-    funcDatabase.StoreUserDatabase(newUser.User)
+	funcDatabase.StoreUserDatabase(newUser.User)
 
 }
 
-func ListenLogin(sessionCookie* string) {
+func ListenLogin(sessionCookie *string) {
 
 	// Listens and Serves New User
 	route := mux.NewRouter()
 	route.HandleFunc("/api/Login", func(w http.ResponseWriter, r *http.Request) {
-        NewLoginResponse(w, r, sessionCookie)
-    }).Methods("POST")
+		NewLoginResponse(w, r, sessionCookie)
+	}).Methods("POST")
 
 	// enables alternate hosts for CORS
 	c := cors.New(cors.Options{
@@ -245,7 +246,7 @@ func ListenLogin(sessionCookie* string) {
 	})
 
 	handler := c.Handler(route)
-    log.Fatal(http.ListenAndServe(":8084", handler))
+	log.Fatal(http.ListenAndServe(":8084", handler))
 
 }
 
@@ -254,25 +255,25 @@ func NewLoginResponse(w http.ResponseWriter, r *http.Request, sessionCookie *str
 	http.SetCookie(w, &http.Cookie{Name: "sessionID", MaxAge: -1, Path: "/"})
 
 	if r.Method != "POST" {
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-        return
-    }
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-    body, err := ioutil.ReadAll(r.Body)
+	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 
-	type LoginUser struct{
+	type LoginUser struct {
 		UserName string `json:"username"`
-    	Password string `json:"password"`
+		Password string `json:"password"`
 	}
 
 	var currUser LoginUser
-	
-    err = json.Unmarshal(body, &currUser)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+
+	err = json.Unmarshal(body, &currUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	activeUser := User{
 		Password: currUser.Password,
@@ -280,17 +281,17 @@ func NewLoginResponse(w http.ResponseWriter, r *http.Request, sessionCookie *str
 	}
 
 	// checks if validate user function returned an empty cookie, if not then setts the cookies
-	if ValidateUser(activeUser) == ""{
+	if ValidateUser(activeUser) == "" {
 		http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
-        return
-	} else{
+		return
+	} else {
 		// Set the cookie
 		cookie := &http.Cookie{
 			Name:     "sessionID",
 			Value:    ValidateUser(activeUser),
 			Path:     "/",
 			HttpOnly: true,
-    		Secure:   false,
+			Secure:   false,
 			SameSite: http.SameSiteLaxMode,
 		}
 		http.SetCookie(w, cookie)
@@ -307,12 +308,12 @@ func NewLoginResponse(w http.ResponseWriter, r *http.Request, sessionCookie *str
 		*sessionCookie = readInCookie.Value
 
 	}
-	
+
 	// fmt.Println(*sessionCookie) // print for testing
 
 }
 
-func ListenForAllPosts(currUser User, cookie string){
+func ListenForAllPosts(currUser User, cookie string) {
 	// all listen functions go in here
 
 	// listens for user login
@@ -326,16 +327,16 @@ func ListenForAllPosts(currUser User, cookie string){
 
 }
 
-func RoutUserPantry(d Database, u User){
-	
+func RoutUserPantry(d Database, u User) {
+
 	// read from .db file and output test user's pantry to frontend
-	for{
+	for {
 		// lock the data
 		dataMutex.Lock()
 
 		var testFoodInterfaceRefresh []interface{}
 		testFoodInterface = testFoodInterfaceRefresh
-		for i := 0; i < len(d.GetUserPantry(u.UserName).FoodInPantry); i++{
+		for i := 0; i < len(d.GetUserPantry(u.UserName).FoodInPantry); i++ {
 			testFoodInterface = append(testFoodInterface, d.GetUserPantry(u.UserName).FoodInPantry[i])
 		}
 
@@ -344,50 +345,41 @@ func RoutUserPantry(d Database, u User){
 
 		// test router
 		programRouter := Router{
-			Name:             "testRouter",
+			Name: "testRouter",
 		}
 		programRouter.RoutPantry("/api/Pantry", ":8080")
 	}
 }
 
-func RoutWeeklyDeals(d Database){
+func RoutWeeklyDeals(d Database) {
 
-		// read from .db file and output test user's pantry to frontend
-		for i := 0; i < len(d.ReadPublixDatabase()); i++{
-			dealsFoodInterface = append(dealsFoodInterface, d.ReadPublixDatabase()[i])
-		}
-		// test router
-		programRouter := Router{
-			Name:             "testRouter",
-		}
-		programRouter.RoutDeals("/api/Deals", ":8081")
+	// read from .db file and output test user's pantry to frontend
+	for i := 0; i < len(d.ReadPublixDatabase()); i++ {
+		dealsFoodInterface = append(dealsFoodInterface, d.ReadPublixDatabase()[i])
+	}
+	// test router
+	programRouter := Router{
+		Name: "testRouter",
+	}
+	programRouter.RoutDeals("/api/Deals", ":8081")
 
 }
 
-func RoutRecommendedRecipes(d Database, currUser User){
+func RoutRecommendedRecipes(d Database, currUser User) {
 
 	userRecList := BestRecipes(d.GetUserPantry(currUser.UserName), d.ReadRecipes(), d.ReadPublixDatabase())
-	for i := 0; i < len(userRecList); i++{
+	for i := 0; i < len(userRecList); i++ {
+		// Sends recipe, items in recipe, and recommended deals
 		recipesFoodInterface = append(recipesFoodInterface, userRecList[i])
-		//recipesFoodInterface = append(recipesFoodInterface, "Pantry Data:")
-		/*for j := 0; j < len(userRecList[i].ItemsInPantry); j++{
-			recipesFoodInterface = append(recipesFoodInterface, userRecList[i].ItemsInPantry[j].Name)
-		}*/
-		//recipesFoodInterface = append(recipesFoodInterface, userRecList[i].ItemsInPantry)
-		//recipesFoodInterface = append(recipesFoodInterface, "Publix Data:")
-		/*for k := 0; k < len(userRecList[i].ItemsOnSale); k++{
-			recipesFoodInterface = append(recipesFoodInterface, userRecList[i].ItemsOnSale[k].Name)
-		}*/
-		//recipesFoodInterface = append(recipesFoodInterface, userRecList[i].ItemsOnSale)
 	}
 
 	programRouter := Router{
-		Name:             "testRouter",
+		Name: "testRouter",
 	}
 	programRouter.RoutRecipes("/api/Recipes", ":8082")
 }
 
-func RoutAllData(d Database, currUser User){
+func RoutAllData(d Database, currUser User) {
 
 	// routs Eddie's pantry, lol
 	go RoutUserPantry(d, currUser)
@@ -401,20 +393,20 @@ func RoutAllData(d Database, currUser User){
 
 func UpdateData(d Database, u User) {
 
-    // Lock the mutex to update the data
-    dataMutex.Lock()
-    // Update the global variable with the updated data
-	for i := 0; i < len(d.GetUserPantry(u.UserName).FoodInPantry); i++{
+	// Lock the mutex to update the data
+	dataMutex.Lock()
+	// Update the global variable with the updated data
+	for i := 0; i < len(d.GetUserPantry(u.UserName).FoodInPantry); i++ {
 		testFoodInterface = append(testFoodInterface, d.GetUserPantry(u.UserName).FoodInPantry[i])
 	}
-    // Unlock the mutex
-    dataMutex.Unlock()
+	// Unlock the mutex
+	dataMutex.Unlock()
 }
 
 func deleteAllCookies(w http.ResponseWriter, r *http.Request) {
-    cookies := r.Cookies()
-    for _, cookie := range cookies {
-        cookie.MaxAge = -1
-        http.SetCookie(w, cookie)
-    }
+	cookies := r.Cookies()
+	for _, cookie := range cookies {
+		cookie.MaxAge = -1
+		http.SetCookie(w, cookie)
+	}
 }
