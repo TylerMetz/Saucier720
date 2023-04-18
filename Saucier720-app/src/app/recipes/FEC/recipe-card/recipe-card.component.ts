5d5982c675489d543f922d81fb417106f51a76bc
Tyler Metz <tmbs9963@gmail.com>
2023-04-18 05:29:02 -0400

    Sub recipe cards

--- Saucier720-app/src/app/recipes/FEC/recipe-card/recipe-card.component.ts
+++ Saucier720-app/src/app/recipes/FEC/recipe-card/recipe-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { RecipeService } from 'src/app/core/services/recipes/recipe.service';
 import { lastValueFrom } from 'rxjs';
@@ -7,20 +7,30 @@ import { RecipePost } from 'src/app/core/interfaces/recipe';
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
-  styleUrls: ['./recipe-card.component.scss']
+  styleUrls: ['./recipe-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipeCardComponent implements OnInit {
 
   recipes: RecipePost[] = [];
   currentRecipeIndex: number = 0;
-  currentRecipe: RecipePost = this.recipes[this.currentRecipeIndex];
+  currentRecipe!: RecipePost;
+  @Input() currentIngredients: string[] = [];
+  nextRecipeFollows: any;
+  printedSubRecipeLines: string[] = [];
 
   constructor(
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private cdRef: ChangeDetectorRef
     ) {}
 
     ngOnInit(): void {
       this.populateRecipes();
+      this.updatePrintedSubRecipeLines();
+    }
+
+    ngDoCheck(): void {
+      this.cdRef.detectChanges();
     }
 
     public async populateRecipes(): Promise<void> {
@@ -44,6 +54,9 @@ export class RecipeCardComponent implements OnInit {
             this.recipes = parsedRecipes;
             console.log('Parse', parsedRecipes)
             console.log('Recipes',this.recipes);
+            this.currentRecipe = this.recipes[this.currentRecipeIndex];
+            this.currentIngredients = this.removeQuotesAndBrackets(this.currentRecipe.R.ingredients);
+            console.log(this.currentIngredients)
             break;
         }
       } catch (error) {
@@ -51,11 +64,29 @@ export class RecipeCardComponent implements OnInit {
       }
     }
 
+    private updatePrintedSubRecipeLines() {
+      this.currentIngredients.forEach((ingredient, i) => {
+        if (this.checkForRecipeFollows(ingredient)) {
+          const subIngredients = this.currentIngredients.slice(i + 1);
+          this.printedSubRecipeLines.push(ingredient);
+          subIngredients.forEach(subIngredient => {
+            if (!this.checkForRecipeFollows(subIngredient)) {
+              this.printedSubRecipeLines.push(subIngredient);
+            }
+          });
+        }
+      });
+    }
+
     goToNextRecipe() {
       this.currentRecipeIndex++;
       if (this.currentRecipeIndex >= this.recipes.length) {
         this.currentRecipeIndex = 0;
       }
+      this.currentRecipe = this.recipes[this.currentRecipeIndex];
+      this.currentIngredients = this.removeQuotesAndBrackets(this.currentRecipe.R.ingredients);
+      console.log(this.currentIngredients)
+      console.log(this.currentRecipe.R.title)
     }
 
     goToPrevRecipe() {
@@ -63,10 +94,48 @@ export class RecipeCardComponent implements OnInit {
       if (this.currentRecipeIndex < 0) {
         this.currentRecipeIndex = this.recipes.length - 1;
       }
+      this.currentRecipe = this.recipes[this.currentRecipeIndex];
+      this.currentIngredients = this.removeQuotesAndBrackets(this.currentRecipe.R.ingredients);
+      console.log(this.currentIngredients)
+      console.log(this.currentRecipe.R.title)
     }
 
     checkForRecipeFollows(ingredient: string): boolean {
-      const pattern = /, recipe follows,$/;
+      const pattern = /\brecipe\s+follows\b/i;
+      console.log('recipe follows',pattern.test(ingredient))
       return pattern.test(ingredient);
     }
+    
+
+    getSubIngredients(): string[][] {
+  const subIngredients: string[][] = [];
+  let currentSubIngredients: string[] = [];
+
+  for (let i = 0; i < this.currentIngredients.length; i++) {
+    if (this.checkForRecipeFollows(this.currentIngredients[i])) {
+      // If we've encountered a "recipe follows" line, add the current sub-ingredients
+      // to the sub-ingredients array and start a new one.
+      if (currentSubIngredients.length > 0) {
+        subIngredients.push(currentSubIngredients);
+        currentSubIngredients = [];
+      }
+    } else {
+      // If the current ingredient is not a "recipe follows" line, add it to the current
+      // sub-ingredients array.
+      currentSubIngredients.push(this.currentIngredients[i]);
+    }
+  }
+
+  // Add the final sub-ingredients array to the sub-ingredients array.
+  subIngredients.push(currentSubIngredients);
+
+  return subIngredients;
+}
+
+
+    removeQuotesAndBrackets(arr: string[]): string[] {
+      const regex = /["\[\]]/g; // Matches any occurrence of ", [, or ] globally
+      return arr.map(str => str.replace(regex, '')); // Replace all matches in each string in the array
+    }
+    
 }
