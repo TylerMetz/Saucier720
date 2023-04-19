02c0a49d5a5e608938d45a666d963262fb7e7458
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-04-19 12:18:59 -0400

    test script

--- goTest.sh
+++ goTest.sh
@@ -4,4 +4,12 @@ cd src/TestingFiles
 go test Host_test.go
 go test Scrape_test.go
 go test Db_test.go
+go test CancelGo_test.go
 go test DealsDb_test.go
+go test GenCookie_test.go
+go test Host_test.go
+go test JsonRead_test.go
+go test ListenPost_test.go
+go test RecRecipe_test.go
+go test ShutdownServer_test.go
+go test UpdateDb_test.go
