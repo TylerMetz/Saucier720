7280961bae3279fdcb84483941818e5cddf35381
emenello <edmenello@gmail.com>
2023-04-19 02:40:02 -0400

    updated pantry tests

--- Saucier720-app/cypress/e2e/HttpRequests/pantry.http.spec.cy.ts
+++ Saucier720-app/cypress/e2e/HttpRequests/pantry.http.spec.cy.ts
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import { Ingredient } from "src/app/core/interfaces/ingredient";
 
 context('Network Requests', () => {
   let pantryPageUrl = 'http://localhost:4200/Pantry';
@@ -7,41 +8,45 @@ context('Network Requests', () => {
   beforeEach(() => {
     // cy.visit(pantryPageUrl)
     cy.login();
-    //cy.visit(pantryPageUrl);
+    cy.wait(3000);
+  })
+
+  afterEach(() => {
+    cy.wait(5000);
   })
 
   // Manage HTTP requests in your app
 
-  // it('make a GET request when loading the pantry', () => {
+  it('make a GET request when loading the pantry', () => {
    
-  //   cy.request(pantryGETUrl)
-  //     .should((response) => {
-  //       expect(response.status).to.eq(200)
-  //       // the server sometimes gets an extra comment posted from another machine
-  //       // which gets returned as 1 extra object
-  //       const ingredients: Ingredient[] = response.body as Ingredient[];
+    cy.request(pantryGETUrl)
+      .should((response) => {
+        expect(response.status).to.eq(200)
+        // the server sometimes gets an extra comment posted from another machine
+        // which gets returned as 1 extra object
+        const ingredients: Ingredient[] = response.body as Ingredient[];
 
-  //      expect(ingredients).to.be.an('array').that.is.not.empty;
-  //      expect(ingredients[0]).to.have.property('Name');
-  //      expect(ingredients[0]).to.have.property('Quantity');
+       expect(ingredients).to.be.an('array').that.is.not.empty;
+       expect(ingredients[0]).to.have.property('Name');
+       expect(ingredients[0]).to.have.property('Quantity');
         
-  //       expect(response).to.have.property('headers')
-  //       expect(response).to.have.property('duration')
-  //     })
-  // })
+        expect(response).to.have.property('headers')
+        expect(response).to.have.property('duration')
+      })
+  })
 
-  // it('posting new Pantry Item from pantry form', () => {
+  it('posting new Pantry Item from pantry form', () => {
     
-  //   cy.visit(pantryPageUrl);
-  //   cy.get('app-new-pantry-item-button');
-  //   const name = 'Pear';
+    cy.visit(pantryPageUrl);
+    cy.get('app-new-pantry-item-button');
+    const name = 'Pear';
 
-  //   cy.get('#name').type(name);
-  //   cy.contains('Post').click();
-  //   cy.request(pantryGETUrl)
-  //   cy.reload();
-  //   cy.get('app-pantry-table').contains('Pear');
-  // })
+    cy.get('#name').type(name, {delay: 150});
+    cy.contains('Post').click();
+    cy.request(pantryGETUrl)
+    
+    cy.get('app-pantry-table').contains('Pear');
+  })
 
   it('deleting pantry items', () => {
     cy.visit(pantryPageUrl);
@@ -59,7 +64,7 @@ context('Network Requests', () => {
 
     cy.get('#name').type(name, {delay: 150});
     cy.contains('Post').click();
-    cy.reload();
+    
     cy.request({
       url: pantryGETUrl,
       headers: {
@@ -67,6 +72,7 @@ context('Network Requests', () => {
         'Cookie': cookie
       },
     })
+    
     
 
     cy.get('tr')
@@ -79,7 +85,7 @@ context('Network Requests', () => {
     .contains('UpdatePantry')
     .click();
 
-    cy.reload();
+    
     cy.request({
       url: pantryGETUrl,
       headers: {
@@ -90,9 +96,59 @@ context('Network Requests', () => {
     cy.get('tr')
     .should('not.contain', 'starfruit')
 
-    // cy.contains('tr', 'starfruit')  // find the tr containing the ingredient name
-    // .find('button:contains("Delete")')  // find the delete button within that tr
-    // .click() 
+  })
+
+  it('increasing quantity pantry items', () => {
+    cy.visit(pantryPageUrl);
+    let cookie = cy.getCookie('sessionID')
+    cy.request({
+      url: pantryGETUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': cookie
+      },
+    })
+    const name = 'starfruit';
+
+    cy.get('app-new-pantry-item-button');
+
+    cy.get('#name').type(name, {delay: 150});
+    cy.contains('Post').click();
+    
+    cy.reload();
+    cy.request({
+      url: pantryGETUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': cookie
+      },
+    })
+  
+    cy.get('tr')
+    .contains('starfruit')
+    .parent()
+    .find('button')
+    .contains('+')
+    .click();
+
+    cy.get('button')
+    .contains('UpdatePantry')
+    .click();
+
+
+    cy.visit(pantryPageUrl);
+    cy.request({
+      url: pantryGETUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': cookie
+      },
+    })
+    cy.reload();
+    cy.get('tr')
+    .should('contain', 'starfruit', '2')
+    // cy.get('tr')
+    // .should('contain', '2')
 
   })
 
