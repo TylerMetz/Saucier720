2b14af499260cf3297dd665c399d7349b5f14a2c
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-04-25 14:12:55 -0400

    first attempt to redo routing

---
+++ src/BackendPkg/8080Router.go
@@ -0,0 +1,125 @@
+package BackendPkg
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	_"time"
+	_"github.com/gorilla/mux"
+	_"github.com/rs/cors"
+	_"io/ioutil"
+	"log"
+	_"sync"
+	_"context"
+	//"strings"
+	//"bytes"
+)
+
+// GLOBAL VARIABLES
+var pantryInterface []interface{}
+var dealsInterface []interface{}
+var recipesInterface []interface{}
+var backendDatabase Database
+
+// ALL ROUTING FUNCTIONS
+
+func handlePantry(response http.ResponseWriter, request *http.Request) {
+
+	jsonResponse, jsonError := json.Marshal(pantryInterface)
+
+	if jsonError != nil {
+		fmt.Println("Unable to encode JSON")
+	}
+
+	// fmt.Println(string(jsonResponse)) // used to test
+
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	response.Write(jsonResponse)
+}
+
+func handleDeals(response http.ResponseWriter, request *http.Request) {
+
+	jsonResponse, jsonError := json.Marshal(dealsInterface)
+
+	if jsonError != nil {
+		fmt.Println("Unable to encode JSON")
+	}
+
+	// fmt.Println(string(jsonResponse)) // used to test
+
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	response.Write(jsonResponse)
+}
+
+func handleRecipes(response http.ResponseWriter, request *http.Request) {
+
+	jsonResponse, jsonError := json.Marshal(recipesInterface)
+
+	if jsonError != nil {
+		fmt.Println("Unable to encode JSON")
+	}
+
+	// fmt.Println(string(jsonResponse)) // used to test
+
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	response.Write(jsonResponse)
+}
+
+func formatData(currUser User){
+	// read from .db file and prepare all data to be routed
+
+	// lock the user pantry data
+	dataMutex.Lock()
+
+	// save all user pantry data to global variable
+	var pantryInterfaceRefresh []interface{}
+	pantryInterface = pantryInterfaceRefresh
+	for i := 0; i < len(backendDatabase.GetUserPantry(currUser.UserName).FoodInPantry); i++{
+		pantryInterface = append(pantryInterface, backendDatabase.GetUserPantry(currUser.UserName).FoodInPantry[i])
+	}
+
+	// unlock the data
+	dataMutex.Unlock()
+
+	// save all deals data to global variable
+	for i := 0; i < len(backendDatabase.ReadPublixDatabase()); i++{
+		dealsInterface = append(dealsInterface, backendDatabase.ReadPublixDatabase()[i])
+	}
+
+	// save all recipes data to global variable
+	userRecList := BestRecipes(backendDatabase.GetUserPantry(currUser.UserName), backendDatabase.ReadRecipes(), backendDatabase.ReadPublixDatabase())
+	var recipesInterfaceRefresh []interface{}
+	recipesInterface = recipesInterfaceRefresh
+	for i := 0; i < len(userRecList); i++ {
+		// sends recipes, items in recipe, and deals related 
+		recipesInterface = append(recipesInterface, userRecList[i])
+	}
+}
+
+func routData(currUser User){
+
+	// setup all global variables to be routed
+	formatData(currUser)
+
+	// create server
+	server := http.Server{
+        Addr: ":8080",
+    }
+
+	// handle functions
+    http.HandleFunc("/api/Pantry", handlePantry)
+    http.HandleFunc("/api/Recipes", handleRecipes)
+	http.HandleFunc("/api/Deals", handleDeals)
+
+	// listen and serve on server
+    go func() {
+        if err := server.ListenAndServe(); err != nil {
+            log.Fatal(err)
+        }
+    }()
+
+}
