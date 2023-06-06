c76f5bc5cf59ced6b2cb5fa89d90232783d706e5
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-06-05 22:18:33 -0500

    fixed pricing format

--- src/WalmartScraper.py
+++ src/WalmartScraper.py
@@ -14,14 +14,14 @@ def scrape_walmart():
     
     # Example: Extract all product names and prices from the home page
     products = soup.find_all("span", {"class": "normal dark-gray mb0 mt1 lh-title f6 f5-l lh-copy"})
-    prices = soup.find_all("span", {"class": "w_iUH7"})
+    prices = soup.find_all("div", {"class": "flex flex-wrap justify-start items-center lh-title mb1"})
     
     if len(prices) == 0 & len(products) == 0:
         print("No products found")
     
     for product, price in zip(products, prices):
         print("Product:", product.text.strip())
-        print("Price:", price.text.strip())
+        print("Price:", price.text.strip().split('current', 1)[0])
         print()
         
 def main():
