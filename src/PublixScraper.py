63bbcc545f60ffa69af314efd483fa103145ec1a
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-06-23 11:58:02 -0400

    Managed to open publix

--- src/PublixScraper.py
+++ src/PublixScraper.py
@@ -1,11 +1,13 @@
 import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 import sys
 
 def scrape_publix():
@@ -17,27 +19,23 @@ def scrape_publix():
     else:
         driverPath = "SeleniumDrivers/chromedriver_mac64/chromedriver"
 
-    while True:
-        # Set up Selenium options 
-        options = Options()
-        options.add_argument("--headless")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
+    # Set up Selenium options 
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
 
-        # Start Selenium webdriver
-        driver = webdriver.Chrome(executable_path = driverPath, options = options)
+    driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-        # Open page
-        driver.get(url)
+    # Open page
+    driver.get(url)
 
-        # Wait for page to load 
-        wait = WebDriverWait(driver, 10)
-        print("Waiting for page to load")
-        
-        # Get page source
-        page_source = driver.page_source
-        
+    # Wait for page to load 
+    wait = WebDriverWait(driver, 5)
+    print("Waiting for page to load")
+
+    
 
 def main():
     scrape_publix()
