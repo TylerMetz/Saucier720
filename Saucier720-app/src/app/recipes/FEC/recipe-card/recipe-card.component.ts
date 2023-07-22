b68582ac98bf7a5b03fec9a7f9b854391bdfe183
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-07-21 20:53:29 -0500

    delete button works successfully

--- Saucier720-app/src/app/recipes/FEC/recipe-card/recipe-card.component.ts
+++ Saucier720-app/src/app/recipes/FEC/recipe-card/recipe-card.component.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { RecipeService } from 'src/app/core/services/recipes/recipe.service';
 import { lastValueFrom } from 'rxjs';
 import { RecipePost } from 'src/app/core/interfaces/recipe';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-recipe-card',
@@ -22,7 +23,8 @@ export class RecipeCardComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private cdRef: ChangeDetectorRef
+    private cdRef: ChangeDetectorRef,
+    private cookieService: CookieService
   ) {}
 
   ngOnInit(): void {
@@ -70,13 +72,22 @@ export class RecipeCardComponent implements OnInit {
     }
   }
 
-  getAuthorCreditFromRecipeID(recipeID: string): string {
+  public getAuthorCreditFromRecipeID(recipeID: string): string {
+    // used to get recipe author from recipeID
     const author = recipeID.replace(/\d+/g, '');
-  
     if (author === 'json') {
       return 'MealDealz Classic Recipe';
     } else {
       return 'Created by ' + author;
+    }
+  }
+
+  public isCurrentUserRecipe(recipeID: string): boolean {
+    // used to check if current recipe is made my the current user
+    if (recipeID.replace(/\d+/g, '') === this.cookieService.get("sessionID").replace(/\d+/g, '')){
+      return true
+    } else{
+      return false;
     }
   }
   
@@ -174,6 +185,22 @@ export class RecipeCardComponent implements OnInit {
       } catch (error) {
         console.error(error);
       }
+    }
+  }
+
+  // for delete button
+  async deleteUserRecipe() {
+    try {
+      // make post req
+      const response = await lastValueFrom(this.recipeService.postDeleteUserRecipe(this.currentRecipe.R.recipeID));
+      console.log(response);
+
+      // delete this recipe card and move to the next
+      this.recipes.splice(this.currentRecipeIndex, 1);
+      this.goToNextRecipe();
+      
+    } catch (error) {
+      console.error(error);
     }
   }
 
--- src/BackendPkg/NewRouter.go
+++ src/BackendPkg/NewRouter.go
@@ -822,6 +822,50 @@ func handleRecipeFilters(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func handleDeleteUserRecipe(w http.ResponseWriter, r *http.Request) {
+
+	// verify POST request from frontend
+    if r.Method == "OPTIONS" {
+        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+        w.Header().Set("Access-Control-Allow-Methods", "POST")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
+        w.WriteHeader(http.StatusOK)
+        return
+    }
+
+	// set correct headers
+    w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+    w.Header().Set("Access-Control-Allow-Methods", "POST")
+    w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	// translate POST data to ASCII
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// save ASCII as string
+	recipeID := string(body)
+
+	// change store selection global var
+	UpdatingData = true;
+	backendDatabase.DeleteUserRecipe(recipeID)
+	UpdatingData = false;
+
+	// write a successful header
+	w.WriteHeader(http.StatusOK)
+
+	// if the header was successful, change the recipe data
+	if http.StatusOK == 200 {
+		// get new data for routing
+		UpdateAllData()
+	}
+
+}
+
 func ListenForData(){
 	
 	// handle the listening functions
@@ -858,6 +902,10 @@ func ListenForData(){
 	http.HandleFunc("/api/RecommendedRecipesFilters", func(response http.ResponseWriter, request *http.Request) {
         handleRecipeFilters(response, request)
     })
+	http.HandleFunc("/api/DeleteUserRecipe", func(response http.ResponseWriter, request *http.Request) {
+        handleDeleteUserRecipe(response, request)
+    })
+	
 
 	// create server
 	server := &http.Server{Addr: ":8082"}
