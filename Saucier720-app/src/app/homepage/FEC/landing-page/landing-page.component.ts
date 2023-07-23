3e2b5c9df9a33e371e29ac5191a94d226de3ea3b
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-07-23 18:28:30 -0500

    made him talk

--- Saucier720-app/src/app/homepage/FEC/landing-page/landing-page.component.ts
+++ Saucier720-app/src/app/homepage/FEC/landing-page/landing-page.component.ts
@@ -4,21 +4,21 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 @Component({
     selector: 'app-landing-page',
     templateUrl: './landing-page.component.html',
-    styleUrls:[ './landing-page.component.scss'],
+    styleUrls: ['./landing-page.component.scss'],
     animations: [
-        trigger('mouthTalking', [
-            state('false', style({ transform: 'scaleY(1)' })),
-            state('true', style({ transform: 'scaleY(0.7)' })),
-            transition('false <=> true', animate('200ms ease-out')),
-        ]),
+      trigger('mouthTalking', [
+        state('false', style({ transform: 'scaleY(1)' })),
+        state('true', style({ transform: 'scaleY(0.7)' })),
+        transition('false <=> true', animate('200ms ease-out')),
+      ]),
     ],
   })
-
-export class LandingPageComponent {
+  export class LandingPageComponent {
     isMouthTalking = false;
-
+  
     // Function to toggle the animation state
     toggleAnimation() {
-        this.isMouthTalking = !this.isMouthTalking;
+      this.isMouthTalking = !this.isMouthTalking;
     }
-}
+  }
+  
