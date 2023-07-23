5b69a0829f186dfb82ce2fb089e3f19c13d6087e
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-07-23 17:31:05 -0500

    finished code that determines if it's the user page or generic landing page.

--- Saucier720-app/src/app/homepage/homepage.component.ts
+++ Saucier720-app/src/app/homepage/homepage.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-
 export class HomePageComponent {
 
-    landingPageActive: boolean = true;
-    userDashboardActive: boolean = false;
-    
+// used to determine which component to display
+  landingPageActive: boolean;
+  userDashboardActive: boolean;
+
+  constructor(private appComponent: AppComponent) {
+    this.landingPageActive = !appComponent.getAuthService().isLoggedIn();
+    this.userDashboardActive = appComponent.getAuthService().isLoggedIn();
+  }
 }
