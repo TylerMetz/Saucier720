0fd662525db87e87e3f1d9d17746395c7ba9d46d
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-07-23 18:23:48 -0500

    both halves of image appear

--- Saucier720-app/src/app/homepage/FEC/landing-page/landing-page.component.ts
+++ Saucier720-app/src/app/homepage/FEC/landing-page/landing-page.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { AuthService } from 'src/app/core/services/Auth/auth.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
     selector: 'app-landing-page',
     templateUrl: './landing-page.component.html',
     styleUrls:[ './landing-page.component.scss'],
-    providers: [AuthService]
+    animations: [
+        trigger('mouthTalking', [
+            state('false', style({ transform: 'scaleY(1)' })),
+            state('true', style({ transform: 'scaleY(0.7)' })),
+            transition('false <=> true', animate('200ms ease-out')),
+        ]),
+    ],
   })
 
-  export class LandingPageComponent {
+export class LandingPageComponent {
+    isMouthTalking = false;
 
-  }
+    // Function to toggle the animation state
+    toggleAnimation() {
+        this.isMouthTalking = !this.isMouthTalking;
+    }
+}
--- Saucier720-app/src/app/homepage/FEC/user-dashboard/user-dashboard.component.ts
+++ Saucier720-app/src/app/homepage/FEC/user-dashboard/user-dashboard.component.ts
@@ -4,8 +4,7 @@ import { AuthService } from 'src/app/core/services/Auth/auth.service';
 @Component({
     selector: 'app-user-dashboard',
     templateUrl: './user-dashboard.component.html',
-    styleUrls:[ './user-dashboard.component.scss'],
-    providers: [AuthService]
+    styleUrls:[ './user-dashboard.component.scss']
   })
 
   export class UserDashBoardComponent {
