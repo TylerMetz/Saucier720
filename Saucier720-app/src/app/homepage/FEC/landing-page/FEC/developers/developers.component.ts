5a0a3d4ade0c22bed95881cf8671638245b5507c
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-07-28 23:01:19 -0500

    basic dev profile is done

--- Saucier720-app/src/app/homepage/FEC/landing-page/FEC/developers/developers.component.ts
+++ Saucier720-app/src/app/homepage/FEC/landing-page/FEC/developers/developers.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+interface DeveloperProfile {
+  picture: string;
+  name: string;
+  github: string;
+  linkedin: string;
+}
+
 @Component({
   selector: 'app-developers',
   templateUrl: './developers.component.html',
   styleUrls: ['./developers.component.scss']
 })
 export class DevelopersComponent {
+  developerProfiles: DeveloperProfile[] = [
+    {
+      picture: '../../../../assets/images/dev-profile-pictures/riley-dev-profile.png',
+      name: 'Riley Cleavenger',
+      github: 'https://github.com/rileycleavenger',
+      linkedin: 'https://www.linkedin.com/in/rileycleavenger',
+    },
+    {
+      picture: '../../../../assets/images/dev-profile-pictures/tyler-dev-profile.png',
+      name: 'Tyler Metz',
+      github: 'https://github.com/janesmith',
+      linkedin: 'https://www.linkedin.com/in/janesmith',
+    },
+    // Add more developer profiles here...
+  ];
 
+  currentProfileIndex: number = 0;
+
+  // Function to navigate to the next developer profile
+  nextProfile() {
+    this.currentProfileIndex = (this.currentProfileIndex + 1) % this.developerProfiles.length;
+  }
+
+  // Function to navigate to the previous developer profile
+  previousProfile() {
+    this.currentProfileIndex = (this.currentProfileIndex - 1 + this.developerProfiles.length) % this.developerProfiles.length;
+  }
 }
