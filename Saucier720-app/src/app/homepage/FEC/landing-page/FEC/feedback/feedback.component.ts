08cb64681a17cf67288c686f6bd394a769fce906
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-07-29 22:35:53 -0500

    switches to message

--- Saucier720-app/src/app/homepage/FEC/landing-page/FEC/feedback/feedback.component.ts
+++ Saucier720-app/src/app/homepage/FEC/landing-page/FEC/feedback/feedback.component.ts
@@ -1,32 +1,52 @@
 import { Component } from '@angular/core';
 import { FormService } from 'src/app/core/services/form/form.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.scss']
+  styleUrls: ['./feedback.component.scss'],
 })
 export class FeedbackComponent {
+
 
   name: string = '';
   email: string = '';
   message: string = '';
 
+  showMessage: boolean = false; // Variable to track if the message is shown
+  showForm: boolean = true; // Variable to toggle between form and message
+
   constructor(private formService: FormService) { }
 
   onSubmit() {
-    this.formService.sendFeedback(this.name, this.email, this.message)
-      .subscribe(
-        () => {
-          console.log('Feedback submitted successfully');
-          // Clear the form fields
-          this.name = '';
-          this.email = '';
-          this.message = '';
-        },
-        (error) => {
-          console.error('Failed to submit feedback:', error);
-        }
-      );
+    if (!this.showMessage) {
+      this.showMessage = true; // Show the message
+      this.showForm = false; // Hide the form
+      this.formService.sendFeedback(this.name, this.email, this.message)
+        .subscribe(
+          () => {
+            console.log('Feedback submitted successfully');
+          },
+          (error) => {
+            console.error('Failed to submit feedback:', error);
+          }
+        );
+    } else {
+      // Show the form again and reset the message state
+      this.showForm = true;
+      this.showMessage = false;
+    }
   }
+
+  onResetForm() {
+    // Reset the form fields and set the form state to its initial state
+    this.name = '';
+    this.email = '';
+    this.message = '';
+    this.showForm = true;
+    this.showMessage = false;
+  }
+
 }
+    
