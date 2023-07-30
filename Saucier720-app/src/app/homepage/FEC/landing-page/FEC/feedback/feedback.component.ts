3456ab170f9821d82824cd8c96cae2e65d8b1f34
rileycleavenger <73538882+rileycleavenger@users.noreply.github.com>
2023-07-29 20:43:14 -0500

    it populates a google form now

--- Saucier720-app/src/app/homepage/FEC/landing-page/FEC/feedback/feedback.component.ts
+++ Saucier720-app/src/app/homepage/FEC/landing-page/FEC/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormService } from 'src/app/core/services/form/form.service';
 
 @Component({
   selector: 'app-feedback',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class FeedbackComponent {
 
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+  constructor(private formService: FormService) { }
+
+  onSubmit() {
+    this.formService.sendFeedback(this.name, this.email, this.message)
+      .subscribe(
+        () => {
+          console.log('Feedback submitted successfully');
+          // Clear the form fields
+          this.name = '';
+          this.email = '';
+          this.message = '';
+        },
+        (error) => {
+          console.error('Failed to submit feedback:', error);
+        }
+      );
+  }
 }
