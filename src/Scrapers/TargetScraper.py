488de8240b101165e3b4835e84ef7c76eda12ba8
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-08-03 22:48:43 -0400

    Increased speed and added comments

--- src/Scrapers/TargetScraper.py
+++ src/Scrapers/TargetScraper.py
@@ -16,7 +16,7 @@ def scrape_target():
     
     # Set up Selenium options 
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
@@ -33,34 +33,43 @@ def scrape_target():
     driver.execute_script("document.body.style.zoom='25%'")
     
     # Give time to load
-    time.sleep(10)
+    time.sleep(3)
     scrapePage(driver, True)
     
-    time.sleep(7)
-    # Get every other set of deals
+    time.sleep(1)
+    # Get every other set of deals, stop when page not clickable
     while True:
         try:                                                                                        
             load_more = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#pageBodyContainer > div > div:nth-child(1) > div > div:nth-child(10) > div > div > div > div > div.styles__ProductListGridFadedLoading-sc-u8zdb1-0 > div.styles__StyledRow-sc-wmoju4-0.cbPxiq > div > div.Pagination__RootDiv-sc-sq3l8r-4.bdkChJ > div:nth-child(3) > button")))
             load_more.click()
-            time.sleep(5)
+            time.sleep(1)
             scrapePage(driver, False)
         except:
             break
 
+# Function that actually scrapes
 def scrapePage(driver: webdriver.Chrome, startPage: bool):
+    # Special conditions when it begins
     if startPage:
         driver.execute_script(f"window.scrollBy(0, 1000);")
+    
+    # Need to back up when it is any page but 1
     else:
         driver.execute_script("document.body.style.zoom='25%'")
         driver.execute_script(f"window.scrollBy(0, -500);")
 
-    time.sleep(5)
+    time.sleep(1)
+
+    # Grab page html
     page_source = driver.page_source
 
+    # Create Beautiful Soup Object
     soup = BeautifulSoup(page_source,"html.parser")
 
+    # Grab all cards
     product_cards = soup.find_all('div', {'class': 'hCeGXD'})
 
+    # Grab the product and deal from within each card
     for card in product_cards:
         try:
             product = card.find(attrs={'data-test': 'product-title'}).text
@@ -76,6 +85,7 @@ def scrapePage(driver: webdriver.Chrome, startPage: bool):
             print("Error:", str(e))
             continue
 
+    # Scroll down and select next page, must zoom back in in order to see button
     driver.execute_script(f"window.scrollBy(0, 800);")
     driver.execute_script("document.body.style.zoom='100%'")
 
