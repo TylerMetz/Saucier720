ae71e481111a48b543783b861965f96d086505fb
Tyler Metz <tmbs9963@gmail.com>
2023-09-18 11:48:48 -0400

    commented out

--- src/main.go
+++ src/main.go
@@ -17,53 +17,56 @@ func main() {
 	// open the Azure SQL database
 	BackendPkg.AzureOpenDatabase()
 
-	// Reads recipes dataset in not read in yet and stores in DB
-	programDatabase.WriteJSONRecipes()
-
-	// runs scraper if new deals at publix or walmart
-	programScraper.CheckIfScrapeNewDeals(programDatabase)
-
-	// listen for user in a separate goroutine, and wait for session cookie to be defined
-	go BackendPkg.ListenUserInfo(&sessionCookie, &cookieChanged)
-	for sessionCookie == "" && !cookieChanged {}
 	
-	// always check if cookie is changed
-	go func(){
-		for{
-			if(cookieChanged){
-				// determine session user based on cookies
-				for(BackendPkg.CurrentUser.UserName == prevUser.UserName){
-					if sessionCookie != "" {
-						BackendPkg.CurrentUser, _ = programDatabase.UserFromCookie(sessionCookie)
-						if(prevCookie == sessionCookie){
-							BackendPkg.CurrentUser, _ = programDatabase.UserFromCookie(sessionCookie)
-							break;
-						}
-					}
-				}
-				// store prev user 
-				prevUser = BackendPkg.CurrentUser
 
-				// reset cookie change
-				cookieChanged = false
-			}
-		}
-	}()
 
-	// rout and listen for all data actively with the defined session user
-	go BackendPkg.RoutData()
-	go BackendPkg.ListenForData()
+	// // Reads recipes dataset in not read in yet and stores in DB
+	// programDatabase.WriteJSONRecipes()
 
-	// goroutine to set the previous cookie to the session cookie while the session cookie isn't being changed
-	go func(){
-		for{
-			if(!cookieChanged){
-				prevCookie = sessionCookie
-			}
-		}
-	}()
+	// // runs scraper if new deals at publix or walmart
+	// programScraper.CheckIfScrapeNewDeals(programDatabase)
 
-	// run infinitely
-	for{}
+	// // listen for user in a separate goroutine, and wait for session cookie to be defined
+	// go BackendPkg.ListenUserInfo(&sessionCookie, &cookieChanged)
+	// for sessionCookie == "" && !cookieChanged {}
+	
+	// // always check if cookie is changed
+	// go func(){
+	// 	for{
+	// 		if(cookieChanged){
+	// 			// determine session user based on cookies
+	// 			for(BackendPkg.CurrentUser.UserName == prevUser.UserName){
+	// 				if sessionCookie != "" {
+	// 					BackendPkg.CurrentUser, _ = programDatabase.UserFromCookie(sessionCookie)
+	// 					if(prevCookie == sessionCookie){
+	// 						BackendPkg.CurrentUser, _ = programDatabase.UserFromCookie(sessionCookie)
+	// 						break;
+	// 					}
+	// 				}
+	// 			}
+	// 			// store prev user 
+	// 			prevUser = BackendPkg.CurrentUser
+
+	// 			// reset cookie change
+	// 			cookieChanged = false
+	// 		}
+	// 	}
+	// }()
+
+	// // rout and listen for all data actively with the defined session user
+	// go BackendPkg.RoutData()
+	// go BackendPkg.ListenForData()
+
+	// // goroutine to set the previous cookie to the session cookie while the session cookie isn't being changed
+	// go func(){
+	// 	for{
+	// 		if(!cookieChanged){
+	// 			prevCookie = sessionCookie
+	// 		}
+	// 	}
+	// }()
+
+	// // run infinitely
+	// for{}
 	
 }
