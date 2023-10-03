bc2a4c739be69a7ee290fce9669b5166683d85df
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-03 15:29:17 -0400

    made it headless

--- src/Scrapers/Functioning/TargetScraper.py
+++ src/Scrapers/Functioning/TargetScraper.py
@@ -19,7 +19,7 @@ def scrape_target():
     options = Options()
     options.page_load_strategy = 'eager' 
 
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
