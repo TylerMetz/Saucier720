e847d386288a3f9db4942658b8c7c3a6ce626146
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-03 15:29:48 -0400

    made aldi headless

--- src/Scrapers/Functioning/AldiScraper.py
+++ src/Scrapers/Functioning/AldiScraper.py
@@ -18,7 +18,7 @@ def scrape_aldi():
     options = Options()
     options.page_load_strategy = 'eager' 
     
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
