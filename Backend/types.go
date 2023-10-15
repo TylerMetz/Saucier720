e805d3cb2acdd94aac59d2ba7defeacad3216599
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-10-15 17:09:26 -0400

    Structs for Signup & Login requests&response

--- Backend/types.go
+++ Backend/types.go
@@ -3,10 +3,31 @@ package main
 import "time"
 
 type Account struct {
-	UserName   string `json:"UserName`
-	FirstName  string `json:"FirstName"`
-	LastName   string `json:"LastName"`
-	Email      string `json:"Email"`
-	Password   string `json:"Password"`
-	DateJoined time.Time
+	UserName   	string `json:"UserName`
+	FirstName  	string `json:"FirstName"`
+	LastName   	string `json:"LastName"`
+	Email      	string `json:"Email"`
+	Password   	string `json:"Password"`
+	DateJoined 	time.Time
+}
+
+type SignupRequest struct {
+	UserName   	string `json:"UserName`
+	FirstName  	string `json:"FirstName"`
+	LastName   	string `json:"LastName"`
+	Email      	string `json:"Email"`
+	Password   	string `json:"Password"`
+}
+
+type SignupResponse struct {
+	Response	string `json:"Response"`
+}
+
+type LoginRequest struct{
+	UserName	string `json:"UserName"`
+	Password 	string `json:"Password"`
+}
+
+type LoginResponse struct {
+	Cookie		string `json:"Cookie"`
 }
