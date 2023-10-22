001fae16e328a4ce84096e9540058e29e41a9e2e
rileycleavenger <riley.cleavenger@ufl.edu>
2023-10-22 16:46:08 -0400

    scrapers api folder made for function app

---
+++ scrapers-api/scrape_publix/PublixScraper.py
@@ -0,0 +1,104 @@
+import re
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+class Deal:
+    def __init__(self, deal_details, product_name):
+        self.deal_details = deal_details
+        self.product_name = product_name
+
+
+def scrape_publix():
+    # Get to publix website 
+    url = "https://www.publix.com/savings/weekly-ad/view-all"
+    
+    # Set up Selenium options 
+    options = Options()
+    options.page_load_strategy = 'eager' 
+    
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
+    options.add_argument("--disable-geolocation")
+    options.add_experimental_option("prefs", {"profile.default_content_setting_values.geolocation": 2})
+
+    # Installs driver depending on browser
+    driver = webdriver.Chrome(options=options)
+    
+    # Open page
+    driver.get(url) 
+    time.sleep(10)
+    
+    # Select location 
+    location_button = driver.find_element(By.CSS_SELECTOR, "#main > div.savings-content-wrapper.skeleton-spacer > div > div.savings-container.full-bleed > div > div > button > span")
+    
+    # Click button and allow time to load 
+    location_button.click()
+    time.sleep(5)
+
+    # Click search bar
+    search_bar = driver.find_element(By.CSS_SELECTOR,"#navBar > div > div.navigation-bar-main > div > div > div.navigation-section.top > div.user-navigation > div > div > div.navigation-sidebar-container > div.navigation-sidebar-body > div > div > div > div.search-container > form > input[type=search]")
+    search_bar.click()
+    search_bar.send_keys("32601")
+    search_bar.send_keys(Keys.ENTER)
+    time.sleep(5)
+
+    # Click first store
+    store_button = driver.find_element(By.CSS_SELECTOR,"#\\31 560 > div > div > div.buttons-links > div.p-button-group__wrapper.buttons-wrapper > div > button")
+    store_button.click()
+    time.sleep(10)
+    
+    # Click load more
+    while True:
+        try:
+            time.sleep(5)
+            load_more = driver.find_element(By.CSS_SELECTOR,"#main > div.savings-content-wrapper > div > div.savings-container > div.card-loader.savings-content.search-results-section.-coupons > div.button-container > button")
+            load_more.click()
+        except:
+            break
+
+    time.sleep(5)
+    
+    # Now we have the entire page as a string 
+    page_source = driver.page_source
+    # print(page_source)
+
+    # Create Beautifulsoup obj to parse page source
+    soup = BeautifulSoup(page_source, "html.parser")
+
+    # Extract desired data from soup object 
+    products = soup.find_all("div", class_="aspect-ratio-content")
+    # Class changed from "p-text paragraph-sm strong context--default color--null"
+    deals = soup.find_all("span", class_="p-text paragraph-sm normal context--default color--null")
+
+    # Removes <li> tags because they share the same class
+    for deal in deals[:]:
+        if deal.find_parents("li"):
+            deals.remove(deal)
+
+    # Print product and deals 
+    output_deals = []
+
+    pattern = r'alt="(.*?)"'
+    for product, deal in zip(products, deals):
+        product_clean = re.search(pattern, str(product)).group(1)
+        # print("Product: ", product_clean)
+        # print("Deal: ", deal.text.strip())
+        # print()
+        output_deals.append(Deal(deal.text.strip(), product_clean))
+
+    driver.quit() 
+
+    return output_deals
+
+    
+
---
+++ scrapers-api/scrape_publix/function_app.py
@@ -0,0 +1,17 @@
+import logging
+import azure.functions as func
+from . import PublixScraper
+
+app = func.FunctionApp()
+
+@app.schedule(schedule="0 0 5 * * 4", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def scrape_publix(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
+
+    deals = PublixScraper.scrape_publix()
+
+    logging.info('Scraping complete.')
