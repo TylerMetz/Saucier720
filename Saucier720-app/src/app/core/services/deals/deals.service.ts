14e19720a3277c63b0d21698120eb1383acd3198
Sam Forstot <92872780+samforstot@users.noreply.github.com>
2023-10-24 16:34:10 -0400

    service getter

--- Saucier720-app/src/app/core/services/deals/deals.service.ts
+++ Saucier720-app/src/app/core/services/deals/deals.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http';
-import { Ingredient } from '../../interfaces/ingredient';
+import { HttpClient, HttpRequest, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Deals, Ingredient } from '../../interfaces/ingredient';
 import { Store } from '../../interfaces/store';
 import { DEALS } from 'src/app/mocks/deals.mock';
 import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import MealDealzRoutes from '../../interfaces/routes';
 
 @Injectable({
   providedIn: 'root'
@@ -14,25 +16,33 @@ export class DealsService {
 
   constructor(private http: HttpClient, private cookieService: CookieService) { }
 
-  getDeals() {
-    const req = new HttpRequest('GET', this.dealsUrl, { 
-      reportProgress: true
-    });
+  getDeals(store: string): Observable<Deals> {
+    console.log('store: ', store);
+    const options = store ?
+    { params: new HttpParams().set('store', store)} : {};
+    console.log('deals request store: ', store);
+    console.log('deals request options: ', options);
+    return this.http.get<Deals>(MealDealzRoutes.getDealsbyStoreUrl, options);
+  }
+  // getDeals() {
+  //   const req = new HttpRequest('GET', this.dealsUrl, { 
+  //     reportProgress: true
+  //   });
     
-    return this.http.request(req);
-  }
+  //   return this.http.request(req);
+  // }
 
-  postStore(store: Store) {
-    const headers = new HttpHeaders({ 
-      'Content-Type': 'application/json', 
-    });
+  // postStore(store: Store) {
+  //   const headers = new HttpHeaders({ 
+  //     'Content-Type': 'application/json', 
+  //   });
   
-    const body = { store };
-    console.log(body)
-    return this.http.post<any>(this.storePostUrl, body, { headers, withCredentials: true });
-  }
+  //   const body = { store };
+  //   console.log(body)
+  //   return this.http.post<any>(this.storePostUrl, body, { headers, withCredentials: true });
+  // }
 
-  getMockDeals(): Array<Ingredient> {
-    return DEALS;
-  }
+  // getMockDeals(): Array<Ingredient> {
+  //   return DEALS;
+  // }
 }
